430c463ed38fd877f1860d892ac00abb20e69d65
Mike Hansen <web.mhansen@gmail.com>
2015-04-29 17:13:46 -0600

    Typo, xcolor, and some code sniffer stuff

--- inc/notifications.php
+++ inc/notifications.php
@@ -39,6 +39,7 @@ function mm_notification_api( $args = array() ) {
 	        "id": "786f1az11",
 	        "message": "<div style=\'border-radius: 5px;color: #fff; padding: 15px;\'>Hello there, you should buy a theme.</div>",
 	        "background": "url("http://test.com") 50% 50%",
+	        "xcolor" : "#000"
 	        "expires": 1804925514,
 	        "category": {
 	            "name": "Special Offers",
@@ -58,7 +59,7 @@ function mm_notification_api( $args = array() ) {
 
 function mm_notification_deadline( $exp ) {
 	$current_time = time();
-	if( $exp < $current_time ) {
+	if ( $exp < $current_time ) {
 		return false;
 	}
 	$deadline = $exp - $current_time;
@@ -70,10 +71,10 @@ function mm_notification_check_dismissed( $id ) {
 }
 
 function mm_load_notifications() {
-	if( ! get_transient( 'mm_notification', false ) ) {
-		if( $notice = mm_notification_api() ) {
+	if ( ! get_transient( 'mm_notification', false ) ) {
+		if ( $notice = mm_notification_api() ) {
 			$expires = $notice->notification->expires;
-			if( $expiration = mm_notification_deadline( $expires ) ) {
+			if ( $expiration = mm_notification_deadline( $expires ) ) {
 				set_transient( 'mm_notification', $notice, $expiration );
 			}
 		}
@@ -85,18 +86,18 @@ function mm_build_notification( $notice, $id ) {
 	if ( ! empty( $notice->background ) ) {
 		$background = " style='background: " . wp_kses( $notice->background, 'strip' ) . "'";
 	} else {
-		$background = "";
+		$background = '';
 	}
 
-	$output = "<div class='mojo-notice' " . $background . ">";
-	$output .= "<a href='" . admin_url( "?mojo-dismiss-notice=" . esc_attr( $id ) ) . "' class='mojo-dismiss dashicons dashicons-dismiss' alt='dismiss'></a>";
+	$output = "<div class='mojo-notice' " . $background . '>';
+	$output .= "<a style='" . ( isset( $notice->xcolor ) ? "color: " . $notice->xcolor : 'color: #000' ) . "' href='" . admin_url( "?mojo-dismiss-notice=" . esc_attr( $id ) ) . "' class='mojo-dismiss dashicons dashicons-dismiss' alt='dismiss'></a>";
 	$output .= wp_kses_post( $notice->message );
 	$output .= "</div>";
 	return $output;
 }
 
 function mm_dismiss_notice() {
-	if( isset( $_GET['mojo-dismiss-notice'] ) ) {
+	if ( isset( $_GET['mojo-dismiss-notice'] ) ) {
 		$id = esc_attr( $_GET['mojo-dismiss-notice'] );
 		$dismissed_notices = get_option( 'mm_notice_dismissed' );
 		$dismissed_notices[ $id ] = true;
@@ -109,15 +110,15 @@ add_action( 'admin_init', 'mm_dismiss_notice' );
 
 function mm_display_notifications() {
 	$page = $_SERVER['REQUEST_URI'];
-	if( $notice = get_transient( 'mm_notification', false ) ) {
+	if ( $notice = get_transient( 'mm_notification', false ) ) {
 		$dismissed_notices = get_option( 'mm_notice_dismissed', false );
 		$locations = $notice->notification->location;
 
-		if( ! isset( $dismissed_notices[ $notice->notification->id ] ) && property_exists( $locations, 'banner' ) ) {
+		if ( ! isset( $dismissed_notices[ $notice->notification->id ] ) && property_exists( $locations, 'banner' ) ) {
 			$locations->banner;
-			foreach( $locations->banner as $matcher ) {
+			foreach ( $locations->banner as $matcher ) {
 				$matcher = str_replace( "?", "\?", $matcher );
-				if( preg_match( "/" . $matcher . "/i", $page ) ) {
+				if ( preg_match( "/" . $matcher . "/i", $page ) ) {
 					echo mm_build_notification( $notice->notification, $notice->notification->id );
 					return;
 				}
@@ -128,10 +129,10 @@ function mm_display_notifications() {
 add_action( 'admin_notices', 'mm_display_notifications' );
 
 function mm_display_backup_reminder() {
-	if( 'update-core.php' == basename( $_SERVER['REQUEST_URI'] ) ) {
+	if ( 'update-core.php' == basename( $_SERVER['REQUEST_URI'] ) ) {
 		?>
 		<div class="updated">
-			<p>Interested in having you backups setup for you? <a href='<?php echo mm_build_link( 'https://www.mojomarketplace.com/item/backup-your-wordpress-website', array( 'utm_content'=> 'update_backup_notice' ) ); ?>'>learn more</a></p>
+			<p>Interested in having your backups setup for you? <a href='<?php echo mm_build_link( 'https://www.mojomarketplace.com/item/backup-your-wordpress-website', array( 'utm_content' => 'update_backup_notice' ) ); ?>'>learn more</a></p>
 		</div>
 		<?php
 	}
