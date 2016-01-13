be514cd65d260a59c63d03080f383943302c2184
Mike Hansen <web.mhansen@gmail.com>
2016-01-13 11:22:45 -0700

    Introduce Marketplace plugins the the plugin

--- inc/menu.php
+++ inc/menu.php
@@ -6,7 +6,7 @@ function mm_main_menu() {
 add_action( 'admin_menu', 'mm_main_menu' );
 
 function mm_preview_menu() {
-	add_submenu_page( null, 'MOJO Theme Preview', 'MOJO Theme Preview', 'manage_options', 'mojo-theme-preview', 'mm_theme_preview_page' );
+	add_submenu_page( null, 'Theme Preview', 'Theme Preview', 'manage_options', 'mojo-theme-preview', 'mm_theme_preview_page' );
 }
 add_action( 'admin_menu', 'mm_preview_menu' );
 
@@ -24,31 +24,40 @@ function mm_menu_icon() {
 add_action( 'admin_head','mm_menu_icon' );
 
 function mm_add_tool_bar_items( $admin_bar ) {
-	if( current_user_can( 'manage_options' ) ) {
+	if ( current_user_can( 'manage_options' ) ) {
 		$admin_bar->add_menu( array(
-			'id' => 'mojo-marketplace',
-			'title' => 'MOJO Marketplace',
-			'href' => admin_url( 'admin.php?page=mojo-themes' ),
-			'meta' => array(
-				'title' => __( 'MOJO Marketplace' )
+			'id'    => 'mojo-marketplace',
+			'title' => 'Marketplace',
+			'href'  => admin_url( 'admin.php?page=mojo-themes' ),
+			'meta'  => array(
+				'title' => __( 'Marketplace' )
 			),
 		) );
 		$admin_bar->add_menu( array(
-			'id' => 'mojo-themes',
-			'title' => 'MOJO Themes',
-			'parent'=> 'mojo-marketplace',
-			'href' => admin_url( 'admin.php?page=mojo-themes' ),
-			'meta' => array(
-				'title' => __( 'MOJO Themes' )
+			'id'     => 'mojo-themes',
+			'title'  => 'Themes',
+			'parent' => 'mojo-marketplace',
+			'href'   => admin_url( 'admin.php?page=mojo-themes' ),
+			'meta'   => array(
+				'title' => __( 'Themes' )
 			),
 		) );
 		$admin_bar->add_menu( array(
-			'id' => 'mojo-services',
-			'title' => 'MOJO Services',
-			'parent'=> 'mojo-marketplace',
-			'href' => admin_url( 'admin.php?page=mojo-services' ),
-			'meta' => array(
-				'title' => __( 'MOJO Services' )
+			'id'     => 'mojo-services',
+			'title'  => 'Services',
+			'parent' => 'mojo-marketplace',
+			'href'   => admin_url( 'admin.php?page=mojo-services' ),
+			'meta'   => array(
+				'title' => __( 'Services' )
+			),
+		) );
+		$admin_bar->add_menu( array(
+			'id'     => 'mojo-plugins',
+			'title'  => 'Plugins',
+			'parent' => 'mojo-marketplace',
+			'href'   => admin_url( 'admin.php?page=mojo-plugins' ),
+			'meta'   => array(
+				'title' => __( 'Services' )
 			),
 		) );
 	}
@@ -56,10 +65,20 @@ function mm_add_tool_bar_items( $admin_bar ) {
 add_action( 'admin_bar_menu', 'mm_add_tool_bar_items', 100 );
 
 function mm_services_menu() {
-	add_submenu_page( 'mojo-themes', 'MOJO Services', 'MOJO Services', 'manage_options', 'mojo-services', 'mm_services_page' );
+	add_submenu_page( 'mojo-themes', 'Services', 'Services', 'manage_options', 'mojo-services', 'mm_services_page' );
 }
 add_action( 'admin_menu', 'mm_services_menu' );
 
 function mm_services_page() {
 	mm_require( MM_BASE_DIR . 'pages/mojo-services.php' );
+}
+
+function mm_plugins_menu() {
+	add_submenu_page( 'mojo-themes', 'Plugins', 'Plugins', 'manage_options', 'mojo-plugins', 'mm_plugins_page' );
+	add_plugins_page( 'MOJO Plugins', 'MOJO Plugins', 'manage_options', 'plugins-mojo', 'mm_plugins_page' );
+}
+add_action( 'admin_menu', 'mm_plugins_menu' );
+
+function mm_plugins_page() {
+	mm_require( MM_BASE_DIR . 'pages/mojo-plugins.php' );
 }
---
+++ pages/mojo-plugins.php
@@ -0,0 +1,50 @@
+<?php
+$request_url = 'http://www.mojomarketplace.com/api/v2/items';
+$query = http_build_query( array( 'type' => 'plugins', 'category' => 'wordpress' ) );
+$request_url .= '?' . $query;
+$key = md5( $key );
+//if ( false === ( $transient = get_transient( 'mm_api_calls' ) ) || ! isset( $transient[ $key ] ) ) {
+	$transient[ $key ] = wp_remote_get( $request_url );
+	if ( ! is_wp_error( $transient[ $key ] ) ) {
+		set_transient( 'mm_api_calls', $transient, DAY_IN_SECONDS );
+	}
+//}
+$plugins = $transient[ $key ];
+?>
+<div class="wrap">
+	<h2><?php echo apply_filters( 'mm_before_page_title', "" ); ?> MOJO Marketplace - Plugins <a class="add-new-h2" target="_blank" href="<?php echo mm_build_link( 'https://www.mojomarketplace.com/plugins/wordpress', array( 'utm_medium' => 'plugin_admin', 'utm_content' => 'browse_all_plugins_button' ), 'browse_all_plugins' ); ?>">Browse All Plugins &rarr;</a></h2>
+<?php
+if ( ! isset( $transient[ $key ] ) ) {
+	echo "<div class='error'><p>Unable to fetch plugins. The API may be down. Please try again later.</p></div>";
+} else {
+	?>
+	<div class="plugins plugin-browser">
+	<?php
+	$items = json_decode( $plugins['body'] );
+	$items = $items->items;
+	foreach ( $items as $item ) {
+		$item->name = apply_filters( 'mm_item_name', $item->name );
+		?>
+		<div class="plugin" tabindex="0">
+			<div class="plugin-screenshot">
+				<a target="_blank" href="<?php echo mm_build_link( $item->page_url, array( 'utm_medium' => 'plugin_admin', 'utm_content' => 'item_' . $api_args['mojo-items'] . '_image_click' ) ); ?>">
+					<img class="mojo-thumbnail" alt="<?php esc_attr_e( $item->name ); ?>" src="<?php echo esc_url( $item->images->preview_url ); ?>">
+				</a>
+			</div>
+			<h3 class="plugin-name"><?php esc_html_e( $item->name ); ?></h3>
+			<div class="mojo-plugin-actions">
+				<form class="buy_now" method="POST" target="_blank" action="<?php echo mm_build_link( "https://www.mojomarketplace.com/cart", array( 'utm_medium' => 'plugin_admin', 'utm_content' => 'item_' . $api_args['mojo-items'] . '_buy_now_button' ) ); ?>" class="buy_now <?php echo 'item_' . $api_args['mojo-items'] . '_buy_now_' . mm_title_to_slug( $item->name ) ;?>">
+					<input type="hidden" name="data[CartItem][item_id]" value="<?php esc_attr_e( $item->id ); ?>"/>
+					<input class="mm-btn-primary" type="submit" value="Buy Now"/>
+				</form>
+
+				<a target="_blank" href="<?php echo mm_build_link( $item->page_url, array( 'utm_medium' => 'plugin_admin', 'utm_content' => 'item_' . $api_args['mojo-items'] . '_details_button' ) ); ?>" class="button button-secondary">Details</a>
+				<div class="price">$<?php esc_html_e( $item->prices->single_domain_license ); ?></div>
+			</div>
+		</div>
+		<?php
+	}
+}
+?>
+	</div>
+</div>
