dcb87cf8ce4d3bddc9ce7a14abce3b02859fbdc8
Mike <web.mhansen@gmail.com>
2016-01-22 14:17:59 -0700

    Remove shortcode generator, may get moved to an affiliate plugin in te future. Leave the shortcode for back-compat.

--- inc/shortcode-generator.php
+++ inc/shortcode-generator.php
@@ -1,109 +1,5 @@
 <?php
 
-function mm_add_button( $icons ) {
-	$img = MM_BASE_URL . "img/mojo-icon-22.png";
-	$id = 'mm_sg_container';
-	$title = '';
-	if( isset( $_SERVER['PHP_SELF'] ) && strpos( $_SERVER['PHP_SELF'], 'post-new.php' ) ||
-		isset( $_SERVER['SCRIPT_NAME'] ) && strpos( $_SERVER['SCRIPT_NAME'], 'post-new.php' ) ||
-		isset( $_SERVER['PHP_SELF'] ) && strpos( $_SERVER['PHP_SELF'], 'post.php' ) ||
-		isset( $_SERVER['SCRIPT_NAME'] ) && strpos( $_SERVER['SCRIPT_NAME'], 'post.php' ) ) {
-		$icons .= "<a class='thickbox button' title='" . $title . "' href='#TB_inline?width=640&inlineId=" . $id . "'><img style='position:relative; bottom: 2px;' src='" . $img . "' /> Shortcodes</a>";
-	} 
-	return $icons;
-}
-add_action( 'media_buttons_context', 'mm_add_button' );
-
-function mm_add_inline_popup_content() {
-?>
-<div id="mm_sg_container" style="display:none;">
-	<div class="mm_sg_header">
-		<img style="margin: 0 auto;" src="<?php echo MM_BASE_URL . "img/mojo-sg-header.png"; ?>" />
-		
-	</div>
-	<form id="mojo-sg-form">
-		<p>I would like to display
-			<select name="mojo-sg-quantity" class="mojo-sg-quantity" style="width:40px;">
-				<?php
-				for ( $i = 1; $i <= apply_filters( 'mm_sg_max_qty_themes', 10 ); $i++ ) { 
-					?>
-				<option value='<?php echo $i; ?>' <?php selected( apply_filters( 'mm_sg_default_qty_themes', 3 ), $i, true ); ?>><?php echo $i; ?></option>
-					<?php
-				}
-				?>
-			</select>
-			<select name="mojo-sg-platform" class="mojo-sg-platform">
-				<option value='wordpress' selected>WordPress</option>
-				<option value='joomla'>Joomla</option>
-				<option value='drupal'>Drupal</option>
-				<option value='magento'>Magento</option>
-				<option value='prestashop'>PrestaShop</option>
-			</select>
-
-			<select name="mojo-sg-type" class="mojo-sg-type">
-				<option value='themes' selected>Themes</option>
-				<option value='plugins'>Plugins</option>
-			</select>
-			<br/>and I want them to be the most 
-			<select name="mojo-sg-items" class="mojo-sg-items">
-				<option value='popular' selected>Popular</option>
-				<option value='recent'>Recent</option>
-			</select> items.
-		</p>
-			<hr/>
-		<p>
-			Want a specific seller?
-			<input type="text" name="mojo-sg-seller" class="mojo-sg-seller" />
-		<br/>
-			Would you like to add your affiliate id?
-			<input type="text" name="mojo-sg-aff" class="mojo-sg-aff" />
-		</p>
-
-		<br/>
-		<br/>
-		<input class="mm-btn-primary" type="submit" value="Insert Into Content" />
-	</form>
-</div>
-<?php
-}
-add_action( 'admin_footer-post-new.php', 'mm_add_inline_popup_content' );
-add_action( 'admin_footer-page-new.php', 'mm_add_inline_popup_content' );
-add_action( 'admin_footer-post.php', 'mm_add_inline_popup_content' );
-add_action( 'admin_footer-page.php', 'mm_add_inline_popup_content' );
-add_action( 'admin_footer-widgets.php', 'mm_add_inline_popup_content' );
-
-
-function mm_sg_js() {
-	?>
-<script type="text/javascript">
-jQuery( document ).ready( function() {
-	function mm_build_shortcode() {
-		var platform = jQuery( '.mojo-sg-platform' ).val(); 
-		var type = jQuery( '.mojo-sg-type' ).val(); 
-		var item = jQuery( '.mojo-sg-items' ).val(); 
-		var quantity = jQuery( '.mojo-sg-quantity' ).val(); 
-		var aff = jQuery( '.mojo-sg-aff' ).val(); 
-		var seller = jQuery( '.mojo-sg-seller' ).val();
-		return "[mojoitem platform='" + platform + "' type='" + type + "' item='" + item + "' quantity='" + quantity + "' seller='" + seller + "' aff='" + aff + "']";
-	}
-	
-	jQuery( '#mojo-sg-form' ).submit( function( e ) {       
-		e.preventDefault();
-		var shortcode = mm_build_shortcode();
-		window.parent.send_to_editor( shortcode );
-		//Close window
-		parent.tb_remove();
-	} );
-} );
-</script>
-	<?php
-}
-add_action( 'admin_footer-post-new.php', 'mm_sg_js' );
-add_action( 'admin_footer-page-new.php', 'mm_sg_js' );
-add_action( 'admin_footer-post.php', 'mm_sg_js' );
-add_action( 'admin_footer-page.php', 'mm_sg_js' );
-add_action( 'admin_footer-widgets.php', 'mm_sg_js' );
-
 function mm_item_shortcode( $atts ) {
 	global $use_mm_styles;
 	$use_mm_styles = true;
@@ -135,7 +31,7 @@ function mm_item_shortcode( $atts ) {
 				<div class="item-photo">
 					<a target="_blank" class="screenshot" href="' . mm_build_link( $item->page_url, array( 'utm_medium' => 'plugin_shortcode', 'utm_content' => 'item_thumbnail', 'r' => $atts['aff'] ) ) . '">
 						<img width="68" height="68" alt="' . $item->name . '" src="' . $item->images->square_thumbnail_url . '">
-					</a>		
+					</a>
 				</div>
 
 				<div class="item-title">
