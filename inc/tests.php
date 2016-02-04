6de4bb4a3fb8dab42355350f81e62d6fc22d8b71
Mike <web.mhansen@gmail.com>
2016-02-04 14:32:13 -0700

    Switch order for test so the get equal amount

--- inc/tests.php
+++ inc/tests.php
@@ -81,13 +81,12 @@ function mm_jetpack_bluehost_only() {
 }
 
 function mm_jpo_test( $file ) {
-	return mm_ab_test_file( 'jetpack-onboarding-v1.1.1', $file, 'vendor/jetpack/jetpack-onboarding/jetpack-onboarding.php', 'tests/jetpack-onboarding/jetpack-onboarding.php', 25, DAY_IN_SECONDS * 90 );
+	return mm_ab_test_file( 'jetpack-onboarding-v1.1.1', $file, 'vendor/jetpack/jetpack-onboarding/jetpack-onboarding.php', 'tests/jetpack-onboarding/jetpack-onboarding.php', 33, DAY_IN_SECONDS * 90 );
 }
 add_filter( 'mm_require_file', 'mm_jpo_test' );
 
 function mm_jpo_test_exempt() {
-	if ( mm_ab_test_inclusion( 'jetpack-onboarding-v1.1.1-exempt', md5( 'jetpack-onboarding-v1-exempt' ), 33, DAY_IN_SECONDS * 90 ) ) {
-		update_option( 'jpo_disabled', 1 );
-	}
+	mm_ab_test_inclusion( 'jetpack-onboarding-v1.1.1-exempt', md5( 'jetpack-onboarding-v1-exempt' ), 25, DAY_IN_SECONDS * 90 );
 }
 add_action( 'init', 'mm_jpo_test_exempt', 9 );
+
