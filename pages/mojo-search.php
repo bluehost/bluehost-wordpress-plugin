92f89226a46fd53c2cc7161fa4962ba83669ed74
Mike <web.mhansen@gmail.com>
2016-02-26 15:09:05 -0700

    Move the header out to a template file. May move it again later.

--- pages/mojo-search.php
+++ pages/mojo-search.php
@@ -15,58 +15,7 @@ if ( ! is_wp_error( $response ) ) {
 	$type = 'search';
 ?>
 <div id="mojo-wrapper">
-	<header id="header" class="navbar navbar-default">
-		<div class="header-block bg-cover" style="background-image: url('<?php echo MM_BASE_URL; ?>img/tmp/header-bg-001.jpg');">
-			<span data-srcset="<?php echo MM_BASE_URL; ?>img/tmp/header-bg-001.jpg, <?php echo MM_BASE_URL; ?>img/tmp/header-bg-001-2x.jpg 2x"></span>
-			<nav>
-				<div class="container">
-					<div class="inner-holder">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<div class="menu-holder dropdown pull-right" style="visibility:hidden;">
-							<button type="button" class="dropdown-toggle visible navbar-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="visible-xs-inline-block visible-sm-inline-block">Account</span>
-								<span class="sr-only">Toggle menu</span>
-								<span class="icon-bar hidden-sm hidden-xs"></span>
-								<span class="icon-bar hidden-sm hidden-xs"></span>
-								<span class="icon-bar hidden-sm hidden-xs"></span>
-							</button>
-							<div class="dropdown-menu">
-								<div class="avatar-block">
-									<div class="avatar"><a href="#"><i class="icon-avatar"></i></a></div>
-									<div class="name"><a href="#">Jonnyapplseed_6</a></div>
-								</div>
-								<ul class="projects-nav list-unstyled">
-									<li><a href="#">Bumble Beer</a></li>
-									<li><a href="#">Suckerpunch</a></li>
-									<li><a href="#">Livin la vida loca</a></li>
-									<li><a href="#">Fixie</a></li>
-									<li><a href="#">Pepermint Latté</a></li>
-								</ul>
-							</div>
-						</div>
-						<div class="header-holder clearfix">
-							<a class="navbar-brand" href="#">
-								<img src="<?php echo MM_BASE_URL; ?>img/tmp/logo-icon.svg" width="250" height="42" alt="MOJO marketplace">
-							</a>
-							<form class="navbar-form form-inline navbar-right text-right" role="search" action="admin.php" method="GET">
-								<div class="form-group">
-									<input type="hidden" name="page" value="mojo-search" />
-									<input name="search" value="<?php echo esc_attr( $search ); ?>" type="text" class="form-control input-lg" placeholder="Search Themes, Plugins &amp; Services">
-								</div>
-								<button type="submit" class="btn btn-info btn-lg">Search</button>
-							</form>
-						</div>
-					</div>
-				</div>
-				<?php mm_require( MM_BASE_DIR . 'pages/navigation.php' ); ?>
-			</nav>
-		</div>
-	</header>
+	<?php mm_require( MM_BASE_DIR . 'pages/header.php' ); ?>
 	<main id="main">
 		<div class="container">
 			<div class="panel panel-default">
@@ -83,6 +32,7 @@ if ( ! is_wp_error( $response ) ) {
 				<div class="panel-body">
 					<div class="list-group">
 					<?php
+
 					foreach ( $items as $item ) {
 						if ( '0' == $item->prices->single_domain_license ) { continue; }
 						//TODO: this should be a whitelist not a blacklist
--- tests/design-overhaul/mojo-list.php
+++ tests/design-overhaul/mojo-list.php
@@ -6,7 +6,7 @@ $query = array(
 	'type'     => $type,
 	'count'    => 20,
 );
-if ( isset( $_GET['paged'] ) && is_numeric( $_GET['paged']) ) {
+if ( isset( $_GET['paged'] ) && is_numeric( $_GET['paged'] ) ) {
 	$query['page'] = (int) $_GET['paged'];
 } else {
 	$query['page'] = 1;
@@ -48,58 +48,7 @@ if ( ! is_wp_error( $response ) ) {
 
 ?>
 <div id="mojo-wrapper">
-	<header id="header" class="navbar navbar-default">
-		<div class="header-block bg-cover" style="background-image: url( '<?php echo esc_url( MM_BASE_URL . 'img/tmp/header-bg-001.jpg' ); ?>' );">
-			<span data-srcset="<?php echo esc_url( MM_BASE_URL .'img/tmp/header-bg-001.jpg' ); ?>, <?php echo esc_url( MM_BASE_URL . 'img/tmp/header-bg-001-2x.jpg' ); ?> 2x"></span>
-			<nav>
-				<div class="container">
-					<div class="inner-holder">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<div class="menu-holder dropdown pull-right" style="visibility:hidden;">
-							<button type="button" class="dropdown-toggle visible navbar-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="visible-xs-inline-block visible-sm-inline-block">Account</span>
-								<span class="sr-only">Toggle menu</span>
-								<span class="icon-bar hidden-sm hidden-xs"></span>
-								<span class="icon-bar hidden-sm hidden-xs"></span>
-								<span class="icon-bar hidden-sm hidden-xs"></span>
-							</button>
-							<div class="dropdown-menu">
-								<div class="avatar-block">
-									<div class="avatar"><a href="#"><i class="icon-avatar"></i></a></div>
-									<div class="name"><a href="#">Jonnyapplseed_6</a></div>
-								</div>
-								<ul class="projects-nav list-unstyled">
-									<li><a href="#">Bumble Beer</a></li>
-									<li><a href="#">Suckerpunch</a></li>
-									<li><a href="#">Livin la vida loca</a></li>
-									<li><a href="#">Fixie</a></li>
-									<li><a href="#">Pepermint Latté</a></li>
-								</ul>
-							</div>
-						</div>
-						<div class="header-holder clearfix">
-							<a class="navbar-brand" href="#">
-								<img src="<?php echo esc_url( MM_BASE_URL . 'img/tmp/logo-icon.svg' ); ?>" width="250" height="42" alt="MOJO marketplace">
-							</a>
-							<form class="navbar-form form-inline navbar-right text-right" role="search" action="<?php echo esc_url( admin_url( 'admin.php' ) ); ?>" method="GET">
-								<div class="form-group">
-									<input type="hidden" name="page" value="mojo-search" />
-									<input name="search" type="text" class="form-control input-lg" placeholder="Search Themes, Plugins &amp; Services">
-								</div>
-								<button type="submit" class="btn btn-info btn-lg">Search</button>
-							</form>
-						</div>
-					</div>
-				</div>
-				<?php mm_require( MM_BASE_DIR . 'pages/navigation.php' ); ?>
-			</nav>
-		</div>
-	</header>
+	<?php mm_require( MM_BASE_DIR . 'pages/header.php' ); ?>
 	<main id="main">
 		<div class="container">
 			<div class="panel panel-default">
