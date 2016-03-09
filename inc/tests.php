9f0414934281d07c4d02b08079e2dcbdd77407fa
Mike <web.mhansen@gmail.com>
2016-03-09 11:08:51 -0700

    Move tests out to loader file so we do not update this file on every new test

--- inc/tests.php
+++ inc/tests.php
@@ -72,19 +72,5 @@ function mm_themes_categories( $accepted_categories ) {
 add_filter( 'mm_themes_accepted_categories', 'mm_themes_categories' );
 */
 
-/* Start individual tests*/
-function mm_jetpack_bluehost_only() {
-	$host = @exec( 'hostname' );
-	$is_bluehost = ( stripos( $host, 'bluehost' ) ) ? true : false;
-	return $is_bluehost;
-}
-
-function mm_jpo_test( $file ) {
-	return mm_ab_test_file( 'jetpack-onboarding-v1.1.1', $file, 'vendor/jetpack/jetpack-onboarding/jetpack-onboarding.php', 'tests/jetpack-onboarding/jetpack-onboarding.php', 33, DAY_IN_SECONDS * 90 );
-}
-add_filter( 'mm_require_file', 'mm_jpo_test' );
-
-function mm_jpo_test_exempt() {
-	mm_ab_test_inclusion( 'jetpack-onboarding-v1.1.1-exempt', md5( 'jetpack-onboarding-v1-exempt' ), 25, DAY_IN_SECONDS * 90 );
-}
-add_action( 'init', 'mm_jpo_test_exempt', 9 );
+/* Start individual tests */
+mm_require( MM_BASE_DIR . 'tests/loader.php' );
