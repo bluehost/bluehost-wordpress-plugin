09a3e5acf13e247433c93dfaad1eb17fe235fdfa
Mike <web.mhansen@gmail.com>
2016-03-16 09:44:21 -0600

    Remove graphic specific code from services page

--- pages/mojo-services.php
+++ pages/mojo-services.php
@@ -12,7 +12,7 @@ if ( isset( $_GET['paged'] ) && is_numeric( $_GET['paged'] ) ) {
 	$query['page'] = 1;
 }
 
-if ( 'services' == $type || 'graphics' == $type ) {
+if ( 'services' == $type ) {
 	unset( $query['category'] );
 }
 
