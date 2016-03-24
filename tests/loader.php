7575e2c78831c52cb93b07c36b9505617ecd759c
Mike <web.mhansen@gmail.com>
2016-03-24 13:54:40 -0600

    Add the demo to the test loader

--- tests/loader.php
+++ tests/loader.php
@@ -9,3 +9,13 @@ function mm_jpo_test_exempt() {
 	mm_ab_test_inclusion( 'jetpack-onboarding-v1.1.1-exempt', md5( 'jetpack-onboarding-v1-exempt' ), 25, DAY_IN_SECONDS * 90 );
 }
 add_action( 'init', 'mm_jpo_test_exempt', 9 );
+
+function mm_demo_test( $file ) {
+	return mm_ab_test_file( 'mojo-demo-v1', $file, 'pages/mojo-themes.php', 'tests/mojo-themes.php', 33, DAY_IN_SECONDS * 90 );
+}
+add_filter( 'mm_require_file', 'mm_demo_test' );
+
+function mm_demo_test_exempt() {
+	mm_ab_test_inclusion( 'mojo-demo-v1-exempt', md5( 'mojo-demo-v1-exempt' ), 25, DAY_IN_SECONDS * 90 );
+}
+add_action( 'init', 'mm_demo_test_exempt', 9 );
