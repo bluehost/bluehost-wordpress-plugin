cad81f4adeb811208bab96c6861388fee7274bda
Mike <web.mhansen@gmail.com>
2016-04-04 12:45:18 -0600

    Bump version on constant too

--- mojo-marketplace.php
+++ mojo-marketplace.php
@@ -15,7 +15,7 @@ if ( ! defined( 'WPINC' ) ) { die; }
 define( 'MM_BASE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MM_BASE_URL', plugin_dir_url( __FILE__ ) );
 define( 'MM_ASSETS_URL', 'https://www.mojomarketplace.com/mojo-plugin-assets/' );
-define( 'MM_VERSION', '0.8.1' );
+define( 'MM_VERSION', '0.8.2' );
 
 if ( file_exists( MM_BASE_DIR . 'inc/brand.php' ) ) {
 	require_once( MM_BASE_DIR . 'inc/brand.php' );
