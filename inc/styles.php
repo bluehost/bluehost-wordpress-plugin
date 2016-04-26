89e5aeb4db3ef6fee5bb824c7e6abadadd3cb3f3
Mike <web.mhansen@gmail.com>
2016-04-26 15:56:44 -0600

    Allow styles.css to show on all admin pages and main.css only on mojo pages

--- inc/styles.php
+++ inc/styles.php
@@ -1,7 +1,7 @@
 <?php
 function mm_admin_style() {
+	wp_enqueue_style( 'mojo-admin-css', MM_ASSETS_URL . 'css/style.css' );
 	if ( isset( $_GET['page'] ) && false !== strpos( $_GET['page'], 'mojo-' ) ) {
-		wp_enqueue_style( 'mojo-admin-css', MM_ASSETS_URL . 'css/style.css' );
 		wp_enqueue_style( 'mojo-admin-roboto-css', 'https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,500' );
 		wp_enqueue_style( 'mojo-admin-bootstrap-css', MM_ASSETS_URL . 'css/bootstrap.css' );
 		wp_enqueue_style( 'mojo-admin-main-css', MM_ASSETS_URL . 'css/main.css' );
