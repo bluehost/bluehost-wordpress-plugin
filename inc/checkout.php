fa1a7cac280faf9d8f38e4316746e1b6ef1205d1
Mike <web.mhansen@gmail.com>
2016-05-04 14:55:32 -0600

    Add login to base instead of checkout since other features will depend on it

--- inc/checkout.php
+++ inc/checkout.php
@@ -31,22 +31,3 @@ function mm_buy_now() {
 	die;
 }
 add_action( 'wp_ajax_mm_buy_now', 'mm_buy_now' );
-
-function _mm_login(){
-	if ( isset( $_GET['page'] ) && false !== strpos( $_GET['page'], 'mojo-' ) && false == get_transient( '_mm_session_token' ) ) {
-		$args = array(
-			'headers' => array(
-				'AuthType'            => 'Token',
-				'x-api-refresh-token' => get_option( '_mm_refresh_token' ),
-			),
-		);
-		$session = wp_remote_get( 'https://api.mojomarketplace.com/api/v2/user_token', $args );
-		if ( ! is_wp_error( $session ) && isset( $session['response']['code'] ) && 200 == $session['response']['code'] ) {
-			$session_data = json_decode( $session['body'] );
-			if ( property_exists( $session_data, 'status' ) && 'success' == $session_data->status && property_exists( $session_data, 'token' ) ) {
-				set_transient( '_mm_session_token', $session_data->token, HOUR_IN_SECONDS );
-			}
-		}
-	}
-}
-add_action( 'admin_init', '_mm_login' );
