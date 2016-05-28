91bb95ad8d07e4026aeda3e88361d8929d0c5035
Mike <web.mhansen@gmail.com>
2016-05-27 18:34:56 -0600

    Changed my mind again, hashes shoudl be done occasionally so the icon can be updated otherwise we have no way to update them in the future

--- inc/menu.php
+++ inc/menu.php
@@ -1,13 +1,13 @@
 <?php
 
 function mm_main_menu() {
-	$icon_hash = get_option( 'mm_icon_hash', false );
+	$icon_hash = get_transient( 'mm_icon_hash', false );
 	if ( false === $icon_hash ) {
 		$brand = mm_brand();
 		$icon_raw = wp_remote_get( MM_ASSETS_URL . 'img/icons/' . $brand . '-icon.svg' );
 		if ( ! is_wp_error( $icon_raw ) ) {
 			$icon_hash = base64_encode( $icon_raw['body'] );
-			update_option( 'mm_icon_hash', $icon_hash, WEEK_IN_SECONDS * 4 );
+			set_transient( 'mm_icon_hash', $icon_hash, WEEK_IN_SECONDS );
 		}
 	}
 	add_menu_page( 'Marketplace', 'Marketplace', 'manage_options', 'mojo-themes', 'mm_theme_page', 'data:image/svg+xml;base64, ' . $icon_hash, 59 );
