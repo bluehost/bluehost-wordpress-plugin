0431361afd0d893825b9067a98db151a9b8b543c
Mike <web.mhansen@gmail.com>
2016-06-14 14:05:12 -0600

    Remove a hack that was put in place for JPO that has since been corrected

--- inc/base.php
+++ inc/base.php
@@ -16,7 +16,6 @@ function mm_setup() {
 		$events = get_option( 'mm_cron', array() );
 		$events['hourly'][ $event['ea'] ] = $event;
 		update_option( 'mm_cron', $events );
-		update_user_meta( 1, 'show_welcome_screen', 1 );
 	}
 }
 add_action( 'init', 'mm_setup' );
