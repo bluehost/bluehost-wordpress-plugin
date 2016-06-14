12ff9243e87ec43ce890476fcb0698758d2a69ad
Mike <web.mhansen@gmail.com>
2016-06-14 13:50:40 -0600

    Yay! Remove the brand file that causes so much pain

--- mojo-marketplace.php
+++ mojo-marketplace.php
@@ -17,9 +17,6 @@ define( 'MM_BASE_URL', plugin_dir_url( __FILE__ ) );
 define( 'MM_ASSETS_URL', 'https://www.mojomarketplace.com/mojo-plugin-assets/' );
 define( 'MM_VERSION', '0.8.2' );
 
-if ( file_exists( MM_BASE_DIR . 'inc/brand.php' ) ) {
-	require_once( MM_BASE_DIR . 'inc/brand.php' );
-}
 require_once( MM_BASE_DIR . 'inc/base.php' );
 require_once( MM_BASE_DIR . 'inc/checkout.php' );
 require_once( MM_BASE_DIR . 'inc/churn.php' );
