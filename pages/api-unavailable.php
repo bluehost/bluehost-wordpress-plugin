b250fd79966fdcccedbfaffd971c2a87fa6db7d9
Mike <web.mhansen@gmail.com>
2016-06-16 15:17:20 -0600

    Add link to the api failed to load pages

--- pages/api-unavailable.php
+++ pages/api-unavailable.php
@@ -5,7 +5,7 @@
 				<div class="row">
 					<div class="col-xs-12 col-sm-12">
 						<ol class="breadcrumb">
-							<li>Unable to load items from API. Please try again.</li>
+							<li>Unable to load items from API. Please try again. <a href="<?php echo esc_url( add_query_arg( array( 'refresh' => rand( 100, 999 ) ) ) ); ?>">Try Again</a></li>
 						</ol>
 					</div>
 				</div>
