110384be82fd2801c45a2ff1396a1784d3f55c7b
Mike <web.mhansen@gmail.com>
2016-08-25 11:42:45 -0600

    Additional info and simpler button wording

--- pages/modal-destroy-confirm.php
+++ pages/modal-destroy-confirm.php
@@ -14,8 +14,9 @@
 				<div class="col-xs-12 col-sm-12 text-center">
 					<img src="<?php echo esc_url( MM_ASSETS_URL . 'img/destroy-staging.png' ); ?>" />
 					<p style="font-size: 18px;">Are you sure you want to destroy staging?</p>
+					<p>Any changes that are in your staging environment and not deployed will be lost.</p>
 					<button class="btn btn-primary btn-lg mm-close-modal" >Cancel</button>
-					<button class="btn btn-success btn-lg staging-action" data-staging-action="mm_destroy">Yes, I am finished for now</button>
+					<button class="btn btn-success btn-lg staging-action" data-staging-action="mm_destroy">Yes, I am finished</button>
 				</div>
 			</div>
 		</div>
