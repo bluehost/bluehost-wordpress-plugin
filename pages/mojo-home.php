89c8101bab4e381eeec19f0f556d0884dfb53462
Mike <web.mhansen@gmail.com>
2017-06-28 15:11:59 -0600

    Fix pill button style for free.

--- pages/mojo-home.php
+++ pages/mojo-home.php
@@ -94,7 +94,7 @@ require_once( MM_BASE_DIR . 'inc/style_updates.css' );
 
 						<div class="btn-group home-btn-group clearfix panel-body col-sm-12">
 							<a href="<?php echo add_query_arg( array( 'page' => 'mojo-marketplace', 'section' => 'mixed-themes', 'type' => 'premium' ), admin_url( 'admin.php' ) ); ?>" class="btn btn-default btn-md">Premium Themes</a>
-							<a href="<?php echo add_query_arg( array( 'page' => 'mojo-marketplace', 'section' => 'mixed-themes', 'type' => 'free' ), admin_url( 'admin.php' ) ); ?>" class="btn btn-link btn-md">Free Themes</a>
+							<a href="<?php echo add_query_arg( array( 'page' => 'mojo-marketplace', 'section' => 'mixed-themes', 'type' => 'free' ), admin_url( 'admin.php' ) ); ?>" class="btn btn-primary btn-md">Free Themes</a>
 						</div>
 
 					</div>
