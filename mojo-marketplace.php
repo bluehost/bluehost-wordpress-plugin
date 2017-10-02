d536d291456f0d76409af816ece6a5878068bec7
Mike <web.mhansen@gmail.com>
2017-10-02 15:30:46 -0600

    Remove editor prompt in preperation for core to add a warning.

--- mojo-marketplace.php
+++ mojo-marketplace.php
@@ -35,7 +35,6 @@ require_once( MM_BASE_DIR . 'inc/coming-soon.php' );
 require_once( MM_BASE_DIR . 'inc/tests.php' );
 require_once( MM_BASE_DIR . 'inc/performance.php' );
 mm_require( MM_BASE_DIR . 'inc/branding.php' );
-require_once( MM_BASE_DIR . 'inc/editor-prompt.php' );
 mm_require( MM_BASE_DIR . 'inc/sso.php' );
 mm_require( MM_BASE_DIR . 'vendor/jetpack/jetpack-onboarding/jetpack-onboarding.php' );
 if ( mm_jetpack_bluehost_only() ) {
