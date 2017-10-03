cad17829686b9d4e50bc4cc42bb7e8302d2ee3a0
Mike <web.mhansen@gmail.com>
2017-10-02 20:23:16 -0600

    Update plugin title and description when branded.

--- inc/branding.php
+++ inc/branding.php
@@ -16,3 +16,22 @@ function mm_brand( $format = '%s' ) {
 	}
 	return sprintf( $format, sanitize_title( $brand ) );
 }
+
+function mm_plugin_details( $all_plugins ) {
+	$plugin_file = 'mojo-marketplace-wp-plugin/mojo-marketplace.php';
+	if ( isset( $all_plugins[ $plugin_file ] ) ) {
+		switch ( mm_brand() ) {
+			case 'bluehost':
+				$branded_plugin_details = array(
+					'Name'  => 'Bluehost',
+					'Title' => 'Bluehost',
+					'Description' => 'This plugin integrates your WordPress site with the Bluehost control panel, including performance, security, and update features.',
+				);
+				break;
+		}
+		$all_plugins[ $plugin_file ] = wp_parse_args( $branded_plugin_details, $all_plugins[ $plugin_file ] );
+	}
+	return $all_plugins;
+}
+add_filter( 'all_plugins', 'mm_plugin_details' );
+
