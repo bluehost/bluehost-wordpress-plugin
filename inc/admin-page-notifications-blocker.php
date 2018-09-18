a69ea51686b6ea5e5585b03f72d3b818cc3d1499
David Ryan <0aveRyan@users.noreply.github.com>
2018-09-18 08:39:12 -0700

    hide pesky notices on mojo pages using css (#37)
    The code works as intended, but specific CSS has already been added to the plugins' stylesheet that accomplishes the same thing as this code. However, this option is more specific to hiding notifications on plugin admin pages only, so the previous CSS should probably be removed from the stylesheet.

---
+++ inc/admin-page-notifications-blocker.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Only run on mojo pages
+ */
+if ( ! is_admin()
+     || ! isset( $_GET['page'] )
+     || false === stripos( filter_input( INPUT_GET, 'page' ), 'mojo-' )
+) {
+	return;
+}
+
+/**
+ * Class EIG_Admin_Page_Notifications_Blocker
+ */
+class EIG_Admin_Page_Notifications_Blocker {
+	/**
+	 * EIG_Admin_Page_Notifications_Blocker constructor.
+	 */
+	function __construct() {
+		add_action( 'admin_print_styles', array( $this, 'remove_notifications_on_mojo_pages' ) );
+	}
+
+	/**
+	 * Target notices using highly-specific CSS selectors to avoid collisions.
+	 */
+	function remove_notifications_on_mojo_pages() {
+		if ( ! isset( $_GET['page'] ) || false === stripos( filter_input( INPUT_GET, 'page' ), 'mojo-' ) ) {
+			return;
+		}
+		?>
+        <style type="text/css" data-bluehost-hide-notifications="1">
+            #wpbody-content > div.error,
+            #wpbody-content > div.notice,
+            #wpbody-content > .update-nag {
+                display: none !important;
+            }
+        </style>
+		<?php
+	}
+}
+
+new EIG_Admin_Page_Notifications_Blocker();
