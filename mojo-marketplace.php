e39420d80c61b4da7d95a759b61472d03e51cc75
Micah Wood <micah@wpscholar.com>
2019-01-10 12:19:54 -0500

    Address coding standards issues in main plugin file.

--- mojo-marketplace.php
+++ mojo-marketplace.php
@@ -1,6 +1,5 @@
 <?php
-
-/*
+/**
  * Plugin Name: MOJO Marketplace
  * Description: This plugin adds shortcodes, widgets, and themes to your WordPress site.
  * Version: 1.4.2
@@ -8,6 +7,8 @@
  * Author URI: http://mikehansen.me?utm_campaign=plugin&utm_source=mojo_wp_plugin
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package Mojo Marketplace
  */
 
 // Do not access file directly!
@@ -22,28 +23,29 @@ define( 'MM_ASSETS_URL', 'https://www.mojomarketplace.com/mojo-plugin-assets/' )
 
 // Composer autoloader
 if ( version_compare( phpversion(), 5.3, '<' ) ) {
-	require __DIR__ . '/vendor/autoload_52.php';
+	require dirname( __FILE__ ) . '/vendor/autoload_52.php';
 } else {
-	require __DIR__ . '/vendor/autoload.php';
+	require dirname( __FILE__ ) . '/vendor/autoload.php';
 }
 
-require_once( MM_BASE_DIR . 'inc/base.php' );
-require_once( MM_BASE_DIR . 'inc/checkout.php' );
-require_once( MM_BASE_DIR . 'inc/menu.php' );
-require_once( MM_BASE_DIR . 'inc/shortcode-generator.php' );
-require_once( MM_BASE_DIR . 'inc/mojo-themes.php' );
-require_once( MM_BASE_DIR . 'inc/styles.php' );
-require_once( MM_BASE_DIR . 'inc/plugin-search.php' );
-require_once( MM_BASE_DIR . 'inc/jetpack.php' );
-require_once( MM_BASE_DIR . 'inc/user-experience-tracking.php' );
-require_once( MM_BASE_DIR . 'inc/notifications.php' );
-require_once( MM_BASE_DIR . 'inc/staging.php' );
-require_once( MM_BASE_DIR . 'inc/updates.php' );
-require_once( MM_BASE_DIR . 'inc/coming-soon.php' );
-require_once( MM_BASE_DIR . 'inc/tests.php' );
-require_once( MM_BASE_DIR . 'inc/performance.php' );
-require_once( MM_BASE_DIR . 'inc/partners.php' );
+require_once MM_BASE_DIR . 'inc/base.php';
+require_once MM_BASE_DIR . 'inc/checkout.php';
+require_once MM_BASE_DIR . 'inc/menu.php';
+require_once MM_BASE_DIR . 'inc/shortcode-generator.php';
+require_once MM_BASE_DIR . 'inc/mojo-themes.php';
+require_once MM_BASE_DIR . 'inc/styles.php';
+require_once MM_BASE_DIR . 'inc/plugin-search.php';
+require_once MM_BASE_DIR . 'inc/jetpack.php';
+require_once MM_BASE_DIR . 'inc/user-experience-tracking.php';
+require_once MM_BASE_DIR . 'inc/notifications.php';
+require_once MM_BASE_DIR . 'inc/staging.php';
+require_once MM_BASE_DIR . 'inc/updates.php';
+require_once MM_BASE_DIR . 'inc/coming-soon.php';
+require_once MM_BASE_DIR . 'inc/tests.php';
 require_once MM_BASE_DIR . 'inc/track-last-login.php';
+require_once MM_BASE_DIR . 'inc/performance.php';
+require_once MM_BASE_DIR . 'inc/partners.php';
+
 mm_require( MM_BASE_DIR . 'inc/branding.php' );
 if ( mm_jetpack_bluehost_only() ) {
 	$onboard_time = strtotime( get_option( 'mm_install_date', 0 ) ) + DAY_IN_SECONDS * 90;
