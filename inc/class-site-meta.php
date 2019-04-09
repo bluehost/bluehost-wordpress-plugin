1f24b7805a4d84c9f3b806e9d74e914204558c85
Micah Wood <micah@wpscholar.com>
2019-04-09 16:16:07 -0400

    Added a site meta class to provide easy access to site information as used by Bluehost.

---
+++ inc/class-site-meta.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Class for fetching site information as used by Bluehost.
+ *
+ * @package BluehostWordPressPlugin
+ */
+
+/**
+ * Class Bluehost_Site_Meta
+ */
+class Bluehost_Site_Meta {
+
+	/**
+	 * Get the domain for the WordPress installation.
+	 *
+	 * @return string
+	 */
+	public static function get_domain() {
+		return wp_parse_url( get_home_url(), PHP_URL_HOST );
+	}
+
+	/**
+	 * Get the relative filesystem path to the root of the WordPress installation.
+	 *
+	 * @return string
+	 */
+	public static function get_path() {
+		return strstr( ABSPATH, '/public_html/' );
+	}
+
+	/**
+	 * Get the (Bluehost) site ID given the full path to the site root.
+	 *
+	 * @return string Site ID is the path encoded as UTF-8 and converted to hexadecimal.
+	 */
+	public static function get_id() {
+		$path = self::get_path();
+
+		return bin2hex( iconv( mb_detect_encoding( $path, mb_detect_order(), true ), 'UTF-8', $path ) );
+	}
+
+}
