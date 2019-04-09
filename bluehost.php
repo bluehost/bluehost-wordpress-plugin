b4358b08c6b3dc0565aa7d806f709ac5a44c2861
Micah Wood <micah@wpscholar.com>
2019-04-09 16:08:26 -0400

    Replace instances of `MM_BASE_DIR` in the main plugin file with `dirname( __FILE__ )`.

--- bluehost.php
+++ bluehost.php
@@ -46,40 +46,39 @@ if ( is_admin() ) {
 	if ( $did_upgrade ) {
 		update_option( 'bluehost_plugin_version', BLUEHOST_PLUGIN_VERSION, true );
 	}
-
 }
 
 // Require files
-require_once MM_BASE_DIR . 'inc/base.php';
-require_once MM_BASE_DIR . 'inc/checkout.php';
-require_once MM_BASE_DIR . 'inc/menu.php';
-require_once MM_BASE_DIR . 'inc/shortcode-generator.php';
-require_once MM_BASE_DIR . 'inc/mojo-themes.php';
-require_once MM_BASE_DIR . 'inc/styles.php';
-require_once MM_BASE_DIR . 'inc/plugin-search.php';
-require_once MM_BASE_DIR . 'inc/jetpack.php';
-require_once MM_BASE_DIR . 'inc/user-experience-tracking.php';
-require_once MM_BASE_DIR . 'inc/notifications.php';
-require_once MM_BASE_DIR . 'inc/staging.php';
-require_once MM_BASE_DIR . 'inc/updates.php';
-require_once MM_BASE_DIR . 'inc/coming-soon.php';
-require_once MM_BASE_DIR . 'inc/tests.php';
-require_once MM_BASE_DIR . 'inc/track-last-login.php';
-require_once MM_BASE_DIR . 'inc/performance.php';
-require_once MM_BASE_DIR . 'inc/partners.php';
+require_once dirname( __FILE__ ) . '/inc/base.php';
+require_once dirname( __FILE__ ) . '/inc/checkout.php';
+require_once dirname( __FILE__ ) . '/inc/menu.php';
+require_once dirname( __FILE__ ) . '/inc/shortcode-generator.php';
+require_once dirname( __FILE__ ) . '/inc/mojo-themes.php';
+require_once dirname( __FILE__ ) . '/inc/styles.php';
+require_once dirname( __FILE__ ) . '/inc/plugin-search.php';
+require_once dirname( __FILE__ ) . '/inc/jetpack.php';
+require_once dirname( __FILE__ ) . '/inc/user-experience-tracking.php';
+require_once dirname( __FILE__ ) . '/inc/notifications.php';
+require_once dirname( __FILE__ ) . '/inc/staging.php';
+require_once dirname( __FILE__ ) . '/inc/updates.php';
+require_once dirname( __FILE__ ) . '/inc/coming-soon.php';
+require_once dirname( __FILE__ ) . '/inc/tests.php';
+require_once dirname( __FILE__ ) . '/inc/track-last-login.php';
+require_once dirname( __FILE__ ) . '/inc/performance.php';
+require_once dirname( __FILE__ ) . '/inc/partners.php';
 
-mm_require( MM_BASE_DIR . 'inc/branding.php' );
+mm_require( dirname( __FILE__ ) . '/inc/branding.php' );
 if ( mm_jetpack_bluehost_only() ) {
 	$onboard_time = strtotime( get_option( 'mm_install_date', 0 ) ) + DAY_IN_SECONDS * 90;
 	if ( $onboard_time > time() ) {
-		mm_require( MM_BASE_DIR . 'vendor/automattic/jetpack-onboarding/jetpack-onboarding.php' );
-		mm_require( MM_BASE_DIR . 'lib/jetpack-onboarding-tracks/jetpack-onboarding-tracks.php' );
+		mm_require( dirname( __FILE__ ) . '/vendor/automattic/jetpack-onboarding/jetpack-onboarding.php' );
+		mm_require( dirname( __FILE__ ) . '/lib/jetpack-onboarding-tracks/jetpack-onboarding-tracks.php' );
 	}
 }
-mm_require( MM_BASE_DIR . 'updater.php' );
+mm_require( dirname( __FILE__ ) . '/updater.php' );
 // Check proper PHP and bring CLI loader online
 if ( version_compare( PHP_VERSION, '5.3.29' ) >= 0 ) {
-	mm_require( MM_BASE_DIR . 'inc/cli-init.php' );
+	mm_require( dirname( __FILE__ ) . '/inc/cli-init.php' );
 }
 
-mm_require( MM_BASE_DIR . 'inc/admin-page-notifications-blocker.php' );
+mm_require( dirname( __FILE__ ) . '/inc/admin-page-notifications-blocker.php' );
