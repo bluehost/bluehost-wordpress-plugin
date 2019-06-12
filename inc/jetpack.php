f085b866edb0d6ca6dfb9abe7b5e7f4713e38383
Micah Wood <micah@wpscholar.com>
2019-06-12 12:18:48 -0400

    Unregister MailChimp block from JetPack

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -15,3 +15,21 @@ function mm_customize_jetpack_default_modules( $modules ) {
 }
 
 add_filter( 'jetpack_get_default_modules', 'mm_customize_jetpack_default_modules' );
+
+/**
+ * Unregister the MailChimp block.
+ *
+ * @param array $blocks Collection of registered blocks.
+ *
+ * @return array
+ */
+function mm_jetpack_unregister_mailchimp_block( $blocks ) {
+	$found = array_search( 'mailchimp', $blocks, true );
+	if ( false !== $found ) {
+		unset( $blocks[ $found ] );
+	}
+
+	return $blocks;
+}
+
+add_filter( 'jetpack_set_available_blocks', 'mm_jetpack_unregister_mailchimp_block' );
