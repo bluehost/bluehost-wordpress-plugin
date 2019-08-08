05701f65a836af62a0a6e66d6f7c93851e3287ce
William Earnhardt <wearnhardt@gmail.com>
2019-08-08 13:26:34 -0700

    Clean up coming soon notice

--- src/app/pages/home/coming-soon-notice/index.js
+++ src/app/pages/home/coming-soon-notice/index.js
@@ -2,19 +2,30 @@
 /**
  * Internal Dependencies
  */
-import { AppNotice as Notice } from '@/components';
+import { AppButton, AppNotice as Notice } from '@/components';
 
 function ComingSoonNotice() {
-	const comingSoon = window.bluehost.comingSoon;
+	if ( ! window.bluehost.settings.comingSoon ) {
+		return null;
+	}
 	return (
-		<div className="notices">
-			{ comingSoon && (
-			<Notice status="warning" isDismissible={false}>
-				<h2>Your site says "Coming Soon."</h2>
-				<p>Right now, your site is displaying a “coming soon” message. This gives you time to work on your site before you unveil it to the public. </p>
-			</Notice> ) }
-		</div>
-		);
+		<Notice status="warning" isDismissible={false}>
+			<div className="pure-g">
+				<div className="pure-u-1 pure-u-sm-1-2 details">
+					<h2>Your site says “Coming Soon.”</h2>
+					<p>Right now, your site is displaying a “coming soon” message. This gives you time to work on your site before you unveil it to the public. </p>
+				</div>
+				<div className="pure-u-1 pure-u-sm-1-2 action">
+					<AppButton
+					href={ '' }
+					isPrimary
+				>
+					Launch Your Site
+					</AppButton>
+				</div>
+			</div>
+		</Notice>
+	);
 }
 
 export default ComingSoonNotice;
