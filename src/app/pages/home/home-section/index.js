e2f258a2d7c65b384f1c83961a2bff9487d00cd6
David Ryan <reachdavidryan@gmail.com>
2019-08-08 07:19:26 -0700

    Squashed commit of the following:
    commit 2894e0bd120b604ddf14aa1c34475eec4c8fd369
    Author: David Ryan <reachdavidryan@gmail.com>
    Date:   Thu Aug 8 07:14:18 2019 -0700
    
        gen’d assets
    
    commit d019a67e5d75b9343fb7376ac8cfd0b368224345
    Author: David Ryan <reachdavidryan@gmail.com>
    Date:   Wed Aug 7 17:12:54 2019 -0700
    
        hide dupe submenu item
    
    commit fe76ecc9a0ae8ebf6fcd1b1b006539b3761268b6
    Merge: c972cdd ac1883e
    Author: David Ryan <reachdavidryan@gmail.com>
    Date:   Wed Aug 7 17:12:33 2019 -0700
    
        left-menu highlighting nonsense
    
    commit ac1883e1b22b8996a3c45e6036dc4f44a5569c5e
    Merge: cf366e8 358b583
    Author: Micah Wood <micah@wpscholar.com>
    Date:   Wed Aug 7 11:27:56 2019 -0700
    
        Merge branch 'product-card' into micah/updates
    
    commit 358b5836ed29a4580f500ec0e34556c2f0f309f0
    Author: Micah Wood <micah@wpscholar.com>
    Date:   Wed Aug 7 11:25:46 2019 -0700
    
        Remove /assets from repo, added a config.scss with color/font variables, added classnames as a dependency.
    
    commit c972cdd9462e52370ec11e7aae1187ddfd45d0d6
    Author: David Ryan <reachdavidryan@gmail.com>
    Date:   Wed Aug 7 11:01:04 2019 -0700
    
        push navigation progress

--- src/app/pages/home/home-section/index.js
+++ src/app/pages/home/home-section/index.js
@@ -10,18 +10,24 @@ export default class HomeSection extends Component {
 			<section className={ 'bluehost-home-section ' + className }>
 				{ children && (
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Squashed commit of the following:
 				<>
 					<h2>{ title }</h2>
 					<section className="bordered-box">
 						{ children }
 					</section> 
 				</>)
+<<<<<<< HEAD
 =======
 				<h2>{ title }</h2>
 				<section className="bordered-box">
 					{ children }
 				</section> )
 >>>>>>> Initial plugin home page
+=======
+>>>>>>> Squashed commit of the following:
 				}
 			</section>
 		);
