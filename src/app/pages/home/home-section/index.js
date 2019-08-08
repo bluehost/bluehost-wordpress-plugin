3e26a5c50a1b35ec13f911e35781b4d891e94533
William Earnhardt <wearnhardt@gmail.com>
2019-08-08 07:09:05 -0700

    Initial plugin home page

--- src/app/pages/home/home-section/index.js
+++ src/app/pages/home/home-section/index.js
@@ -9,12 +9,19 @@ export default class HomeSection extends Component {
 		return (
 			<section className={ 'bluehost-home-section ' + className }>
 				{ children && (
+<<<<<<< HEAD
 				<>
 					<h2>{ title }</h2>
 					<section className="bordered-box">
 						{ children }
 					</section> 
 				</>)
+=======
+				<h2>{ title }</h2>
+				<section className="bordered-box">
+					{ children }
+				</section> )
+>>>>>>> Initial plugin home page
 				}
 			</section>
 		);
