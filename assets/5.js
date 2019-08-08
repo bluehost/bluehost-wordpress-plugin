fdecffda1b7ac3feeffa3882cfa4e3337f045f54
David Ryan <reachdavidryan@gmail.com>
2019-08-08 07:43:09 -0700

    apply max width to navigation and app-page

--- assets/5.js
+++ assets/5.js
@@ -1,1 +1,38 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{35:function(e,n,t){"use strict";t.r(n);var s=t(0),u=t(3),c=t(10);n.default=function(){return Object(s.createElement)(c.d,null,Object(u.__)("Themes Page","bluehost-wordpress-plugin"))}}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[5],{
+
+/***/ "./src/app/pages/tools/index.js":
+/*!**************************************!*\
+  !*** ./src/app/pages/tools/index.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/element */ "@wordpress/element");
+/* harmony import */ var _wordpress_element__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/components */ "./src/app/components/index.js");
+
+
+/**
+ * WordPress dependencies
+ */
+
+/**
+ * External dependencies
+ */
+
+
+
+var ToolsPage = function ToolsPage() {
+  return Object(_wordpress_element__WEBPACK_IMPORTED_MODULE_0__["createElement"])(_components__WEBPACK_IMPORTED_MODULE_2__["AppPage"], null, Object(_wordpress_i18n__WEBPACK_IMPORTED_MODULE_1__["__"])('Tools Page', 'eig-wp-admin-ui'));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ToolsPage);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=5.js.map
