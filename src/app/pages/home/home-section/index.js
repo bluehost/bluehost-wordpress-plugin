99e293ec4165a81bd973350fe587a41969927e26
David Ryan <reachdavidryan@gmail.com>
2019-08-08 07:35:26 -0700

    resolve conflict

--- src/app/pages/home/home-section/index.js
+++ src/app/pages/home/home-section/index.js
@@ -9,25 +9,12 @@ export default class HomeSection extends Component {
 		return (
 			<section className={ 'bluehost-home-section ' + className }>
 				{ children && (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Squashed commit of the following:
 				<>
 					<h2>{ title }</h2>
 					<section className="bordered-box">
 						{ children }
 					</section> 
 				</>)
-<<<<<<< HEAD
-=======
-				<h2>{ title }</h2>
-				<section className="bordered-box">
-					{ children }
-				</section> )
->>>>>>> Initial plugin home page
-=======
->>>>>>> Squashed commit of the following:
 				}
 			</section>
 		);
