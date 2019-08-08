01b63077f6741b3ec1f3d5ba601516f80d51263b
Micah Wood <micah@wpscholar.com>
2019-08-08 13:44:04 -0700

    Update themes page

--- src/app/pages/marketplace/themes/index.js
+++ src/app/pages/marketplace/themes/index.js
@@ -1,45 +1,73 @@
 /**
  * WordPress dependencies
  */
+import apiFetch from '@wordpress/api-fetch';
 import {Fragment, useState, useEffect} from '@wordpress/element';
 import {__} from '@wordpress/i18n';
+
 /**
  * External dependencies
  */
-import {AppPage, AppSpinner as Spinner, Grid, ProductCard} from '@/components';
-
-import apiFetch from '@wordpress/api-fetch';
+import {
+    AppPage,
+    AppSpinner as Spinner,
+    Dropdown,
+    Grid,
+    Pagination,
+    ProductCard,
+    Search
+} from '@/components';
 
 export default function ThemesPage() {
-    const [isLoading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(false);
     const [items, setItems] = useState([]);
-    apiFetch({path: '/mojo/v1/items'}).then((items) => {
-        console.log(items);
-        setItems(items);
-        setLoading(false);
-    });
+    const [pageCount, setPageCount] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        apiFetch({
+                path: '/mojo/v1/items',
+            }
+        )
+            .then(
+                (response) => {
+                    console.log(response);
+                    setItems(response.items);
+                    setPageCount(response.pageCount);
+                    setLoading(false);
+                }
+            );
+    }, [currentPage]);
+
     return (
         <AppPage>
-            {__('Themes Page', 'bluehost-wordpress-plugin')}
-            <Grid>
-                {
-                    isLoading ? (
-                        <Spinner/>
-                    ) : (
-                        <Fragment>
-                            {items.map(item => {
-                                return (
-                                    <ProductCard
-                                        imageUrl={item.images.thumbnail_url}
-                                        price={item.prices.single_domain_license}
-                                        title={item.name}
-                                    />
-                                );
-                            })}
-                        </Fragment>
-                    )
-                }
-            </Grid>
+            <h1>{__('Premium Themes', 'bluehost-wordpress-plugin')}</h1>
+            <Pagination callback={setCurrentPage} currentPage={currentPage} pageCount={pageCount}/>
+            <Search/>
+            <Dropdown/>
+            {
+                isLoading ? (
+                    <Spinner/>
+                ) : (
+                    <Grid>
+                        {items.map(item => {
+                            return (
+                                <ProductCard
+                                    key={item.id}
+                                    imageUrl={item.images.thumbnail_url}
+                                    price={item.prices.single_domain_license}
+                                    title={item.name}
+                                    isFavorite={false}
+                                />
+                            );
+                        })}
+                    </Grid>
+                )
+            }
+            <div>
+                Ad goes here
+            </div>
+            <Pagination/>
         </AppPage>
     );
 }
