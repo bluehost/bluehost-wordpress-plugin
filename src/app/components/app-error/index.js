403f294340e115455202d5537bb8adf8154ec34c
David Ryan <reachdavidryan@gmail.com>
2019-08-20 15:45:48 -0700

    progress

--- src/app/components/app-error/index.js
+++ src/app/components/app-error/index.js
@@ -1,36 +1,23 @@
 import './style.scss';
 import { __ } from '@wordpress/i18n';
 import { AppButton as Button } from '@/components';
-import { Component } from '@wordpress/element';
 import { ReactComponent as WarningIcon } from '@/assets/warning.svg';
 import { ReactComponent as ErrorStateIllustration } from '@/assets/error-state.svg';
 
-class AppError extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render () {
-        return (
-            <div id="app-error">
-                <div className="warning-icon-wrap">
-                    <WarningIcon className="animated heartBeat" />
-                </div>
-                <h3 className="animated fadeIn">{__( 'There was a problem loading the Bluehost Plugin.')}</h3>
-                <p className="cta-wrap animated fadeIn">{__( 'This usually happens when another plugin is out-of-date or incompatible.')}<br />{__('Get in touch and we\'ll sort out the issue.')}</p>
-                <div className="cta-wrap animated fadeIn">
-                    <Button href="https://bluehost.com/support" isPrimary>{__('Contact Support')}</Button>
-                </div>
-                {this.props.errorLogged && (
-                    <div>
-                        Error logged!.
-                    </div>
-                )}
-                <div className="error-illustration-wrap animated fadeIn">
-                    <ErrorStateIllustration />
-                </div>
-            </div>
-        );
-    }
-}
+const AppError = () => (
+    <div id="app-error">
+        <div className="warning-icon-wrap">
+            <WarningIcon className="animated heartBeat" />
+        </div>
+        <h3 className="animated fadeIn">{__( 'There was a problem loading the Bluehost Plugin.')}</h3>
+        <p className="animated fadeIn">{__('Get in touch and we\'ll sort out the issue for you.')}</p>
+        <div className="animated fadeIn" style={{textAlign: 'center', margin: '1rem auto 80px'}}>
+            <Button href="https://bluehost.com/support" isPrimary>{__('Contact Support')}</Button>
+        </div>
+        <div className="error-illustration-wrap animated fadeIn">
+            <ErrorStateIllustration />
+        </div>
+    </div>
+);
 
 export default AppError;
--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { Snackbar, SnackbarList } from '@wordpress/components';
+import { Animate } from '@wordpress/components';
 import { Component, createRef } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 /**
@@ -35,6 +35,7 @@ class App extends Component {
 		// make refs/this available in
 		this.handleNavFocus = this.handleNavFocus.bind( this );
 		this.handleContentFocus = this.handleContentFocus.bind( this );
+		this.componentDidCatch = this.componentDidCatch.bind( this );
 		this.state = {
 			hasError: false,
 			errorLogged: false
@@ -53,23 +54,19 @@ class App extends Component {
 
 	componentDidCatch(error,info) {
 		this.setState({ hasError: true });
-		const browserResult = browser();
 		axios.post( 
-			'/wp-json/bluehost/v1/error/track', 
+			location.origin + '/wp-json/bluehost/v1/error/track', 
 			qs.stringify({
 				date: new Date(), 
 				message: error.message,
-				browser: browserResult
+				browser: browser(),
+				wpUser: window.userSettings.uid ? window.userSettings.uid : 'Unknown'
 			})
-			).then(function(response) {
-				console.log( 'it the axios response');
-				this.setState({ errorLogged: true });
-			})
+		);
 	}
 
 	render() {
 		if (true === this.state.hasError) {
-
 			return (
 				<div>
 					<AppError errorLogged={this.state.errorLogged} />
@@ -78,25 +75,28 @@ class App extends Component {
 		}
 		return (
 			<div>
-				<Router>
-					<main id="bluehost-app-wrap" className="bluehost-app-wrap animated fadeIn fast">
-						<a className="screen-reader-shortcut bluehost-spa-skip" href="#" onClick={ this.handleNavFocus } onKeyPress={ this.handleNavFocus }>
-							{ __( 'Skip to Navigation', bluehost_i18n ) }
-						</a>
-						<a className="screen-reader-shortcut bluehost-spa-skip" href="#" onClick={ this.handleContentFocus } onKeyPress={ this.handleContentFocus }>
-							{ __( 'Skip to Content', bluehost_i18n ) }
-						</a>
-						<div>
-							<AppHeader />
-						</div>
-						<div id="navigation" tabIndex="-1" ref={ this.navFocus }>
-							<AppPrimaryNav />
-						</div>
-						<div tabIndex="-1" ref={ this.contentFocus }>
-							<AppMain />
-						</div>
-					</main>
-				</Router>
+				<Animate type="appear" options={{ origin: 'center'}}>
+					<Router>
+						{/* <main id="bluehost-app-wrap" className="bluehost-app-wrap animated fadeIn fast"> */}
+						<main id="bluehost-app-wrap" className="bluehost-app-wrap">
+							<a className="screen-reader-shortcut bluehost-spa-skip" href="#" onClick={ this.handleNavFocus } onKeyPress={ this.handleNavFocus }>
+								{ __( 'Skip to Navigation', bluehost_i18n ) }
+							</a>
+							<a className="screen-reader-shortcut bluehost-spa-skip" href="#" onClick={ this.handleContentFocus } onKeyPress={ this.handleContentFocus }>
+								{ __( 'Skip to Content', bluehost_i18n ) }
+							</a>
+							<div>
+								<AppHeader />
+							</div>
+							<div id="navigation" tabIndex="-1" ref={ this.navFocus }>
+								<AppPrimaryNav />
+							</div>
+							<div tabIndex="-1" ref={ this.contentFocus }>
+								<AppMain />
+							</div>
+						</main>
+					</Router>
+				</Animate>
 			</div>
 		);
 	}
