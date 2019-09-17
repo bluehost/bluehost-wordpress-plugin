f718abd0c1a90592f9c72a78fd20c9206d6baba3
Micah Wood <micah@wpscholar.com>
2019-09-17 16:11:30 -0400

    Restructure markup and styling for product cards to match heights properly across grid rows.

--- src/app/components/product-card/index.js
+++ src/app/components/product-card/index.js
@@ -32,32 +32,34 @@ export default function ProductCard(
     return (
         <div className={`product-card ${className}`}>
             <div className="product-card__image" style={style}/>
-            <div className="product-card__details">
-                <div className="product-card__title">{decodeEntities(title)}</div>
-                <div className="product-card__price">{price}</div>
-            </div>
-            <div className="product-card__action-group">
-                <div className={classNames({
-                    'product-card__favorite': true,
-                    'dashicons': true,
-                    'dashicons-star-empty': !isFavorite,
-                    'dashicons-star-filled': isFavorite,
-                    '--is-favorite': isFavorite
-                })} onClick={toggleFavorite}/>
-                <div className="product-card__button-group">
-                    <Button
-                        children={__('View Details', 'bluehost-wordpress-plugin')}
-                        {...buttonSecondary}
-                        className="product-card__button-secondary"
-                        isDefault
-                    />
-                    <Button
-                        children={__('Buy Now', 'bluehost-wordpress-plugin')}
-                        target="_blank"
-                        {...buttonPrimary}
-                        className="product-card__button-primary"
-                        isPrimary
-                    />
+            <div className="product-card__content">
+                <div className="product-card__details">
+                    <div className="product-card__title">{decodeEntities(title)}</div>
+                    <div className="product-card__price">{price}</div>
+                </div>
+                <div className="product-card__action-group">
+                    <div className={classNames({
+                        'product-card__favorite': true,
+                        'dashicons': true,
+                        'dashicons-star-empty': !isFavorite,
+                        'dashicons-star-filled': isFavorite,
+                        '--is-favorite': isFavorite
+                    })} onClick={toggleFavorite}/>
+                    <div className="product-card__button-group">
+                        <Button
+                            children={__('View Details', 'bluehost-wordpress-plugin')}
+                            {...buttonSecondary}
+                            className="product-card__button-secondary"
+                            isDefault
+                        />
+                        <Button
+                            children={__('Buy Now', 'bluehost-wordpress-plugin')}
+                            target="_blank"
+                            {...buttonPrimary}
+                            className="product-card__button-primary"
+                            isPrimary
+                        />
+                    </div>
                 </div>
             </div>
         </div>
