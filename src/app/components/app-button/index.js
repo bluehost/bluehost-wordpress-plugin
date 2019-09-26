623b2d5e5498c95dc92ae4cf2d4abde4afb5b563
Micah Wood <micah@wpscholar.com>
2019-09-26 17:35:30 -0400

    Updated AppButton to support forwarding refs and custom class names

--- src/app/components/app-button/index.js
+++ src/app/components/app-button/index.js
@@ -1,16 +1,26 @@
+import classNames from 'classnames';
+
 /**
  * WordPress dependencies
  */
 import {Button} from '@wordpress/components';
+import {forwardRef} from '@wordpress/element';
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-const AppButton = ({className = '', ...props}) => {
+const AppButton = forwardRef(({className = '', ...props}, ref) => {
     return (
-        <Button {...props} className={`bluehost ${className}`}/>
+        <Button
+            {...props}
+            className={classNames({
+                'bluehost': true,
+                [className]: className.length
+            })}
+            ref={ref}
+        />
     );
-};
+});
 
 export default AppButton;
