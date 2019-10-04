4b61e1d57f8f3f838f02a6d3f91c32ba8847ad70
Micah Wood <micah@wpscholar.com>
2019-10-04 15:50:23 -0400

    Updated location of SVGs and added a few

--- src/app/assets/index.js
+++ src/app/assets/index.js
@@ -1,28 +1,34 @@
 /**
  * External dependencies
  */
-import { ReactComponent as BluehostLogo } from '@/assets/svg/bluehost.svg';
-import { ReactComponent as JetpackLogo } from '@/assets/svg/jetpack.svg';
-
-import { ReactComponent as BillingIcon } from '@/assets/svg/billing.svg';
-import { ReactComponent as HelpIcon } from '@/assets/svg/help.svg';
-import { ReactComponent as ProductsIcon } from '@/assets/svg/products.svg';
-import { ReactComponent as SecurityIcon } from '@/assets/svg/security.svg';
-import { ReactComponent as UserIcon } from '@/assets/svg/user.svg';
-import { ReactComponent as ValidationIcon } from '@/assets/svg/validation.svg';
-
-import { ReactComponent as MenuIcon } from '@/assets/svg/menu.svg';
-import { ReactComponent as CloseIcon } from '@/assets/svg/close.svg';
+import {ReactComponent as BillingIcon} from '@/assets/svg/billing.svg';
+import {ReactComponent as BluehostLogo} from '@/assets/svg/bluehost.svg';
+import {ReactComponent as CloseIcon} from '@/assets/svg/close.svg';
+import {ReactComponent as HelpIcon} from '@/assets/svg/help.svg';
+import {ReactComponent as JetpackLogo} from '@/assets/svg/jetpack.svg';
+import {ReactComponent as MenuIcon} from '@/assets/svg/menu.svg';
+import {ReactComponent as ProductsIcon} from '@/assets/svg/products.svg';
+import {ReactComponent as RadioButtonDisabledIcon} from '@/assets/svg/radio-button-disabled.svg';
+import {ReactComponent as RadioButtonSelectedIcon} from '@/assets/svg/radio-button-selected.svg';
+import {ReactComponent as RadioButtonUnselectedIcon} from '@/assets/svg/radio-button-unselected.svg';
+import {ReactComponent as SecurityIcon} from '@/assets/svg/security.svg';
+import {ReactComponent as UserIcon} from '@/assets/svg/user.svg';
+import {ReactComponent as ValidationIcon} from '@/assets/svg/validation.svg';
+import {ReactComponent as WorkingImage} from '@/assets/svg/working.svg';
 
 export {
-	BillingIcon,
-	BluehostLogo,
-	CloseIcon,
-	HelpIcon,
-	JetpackLogo,
-	MenuIcon,
-	ProductsIcon,
-	SecurityIcon,
-	UserIcon,
-	ValidationIcon,
+    BillingIcon,
+    BluehostLogo,
+    CloseIcon,
+    HelpIcon,
+    JetpackLogo,
+    MenuIcon,
+    ProductsIcon,
+    RadioButtonDisabledIcon,
+    RadioButtonSelectedIcon,
+    RadioButtonUnselectedIcon,
+    SecurityIcon,
+    UserIcon,
+    ValidationIcon,
+    WorkingImage,
 };
