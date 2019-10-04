597d7f6ff3e98b319e3cc11c5a4de28707f5bc23
Micah Wood <micah@wpscholar.com>
2019-10-04 14:17:23 -0400

    Fix width of homepage content to be restricted to 822px max, or 100% otherwise.

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -22,22 +22,21 @@ import HostingSection from './hosting/';
 import ComingSoonNotice from './coming-soon-notice/';
 
 const HomePage = () => (
-	<AppPage className="home">
-		<div className="pure-g">
-			<div className="pure-u-1 pure-u-sm-3-4">
-				<div className="welcome-section">
-					<h1>Welcome to your WordPress site!</h1>
-					<p>From here, you can quickly add content to your site, manage for-sale products, work on your site’s design and performance, manage hosting, and access tools to increase your traffic.</p>
-				</div>
-				<ComingSoonNotice />
-				<ContentSection />
-				<DesignBuildSection />
-				<TrafficEngagementSection />
-				<PerformanceSection />
-				<HostingSection />
-			</div>
-		</div>
-	</AppPage>
+    <AppPage className="home">
+        <div className="restricted-width">
+            <div className="welcome-section">
+                <h1>Welcome to your WordPress site!</h1>
+                <p>From here, you can quickly add content to your site, manage for-sale products, work on your site’s
+                    design and performance, manage hosting, and access tools to increase your traffic.</p>
+            </div>
+            <ComingSoonNotice/>
+            <ContentSection/>
+            <DesignBuildSection/>
+            <TrafficEngagementSection/>
+            <PerformanceSection/>
+            <HostingSection/>
+        </div>
+    </AppPage>
 );
 
 export default HomePage;
