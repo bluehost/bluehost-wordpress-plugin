d69b22141f6f327127c1a7000f4d61e55da99b91
Micah Wood <micah@wpscholar.com>
2019-11-06 15:31:19 -0500

    Tweaks to DropdownButton component to ensure that triggerOnSelect works even if selected item is current.

--- src/app/components/dropdown-button/index.js
+++ src/app/components/dropdown-button/index.js
@@ -22,182 +22,200 @@ import './style.scss';
  * @returns {String}
  */
 export default function DropdownButton(
-    {
-        className = '',
-        defaultOptionIndex = 0,
-        disabled = false,
-        label,
-        options = [],
-        triggerOnSelect = true,
-        width = 200
-    }
+	{
+		className = '',
+		defaultOptionIndex = 0,
+		disabled = false,
+		label,
+		options = [],
+		triggerOnSelect = true,
+		width = 200
+	}
 ) {
 
-    let component = useRef(null);
-    let toggle = useRef(null);
+	let component = useRef(null);
+	let toggle = useRef(null);
 
-    const id = uniqueId('dropdown-button-');
-    const [active, setActive] = useState(null);
-    const [activeDescendant, setActiveDescendant] = useState(null);
-    const [hasFocus, setHasFocus] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
-    const [selected, setSelected] = useState(defaultOptionIndex);
-    const [actionName, setActionName] = useState(get([selected, 'title'], options));
+	const id = uniqueId('dropdown-button-');
+	const [active, setActive] = useState(null);
+	const [activeDescendant, setActiveDescendant] = useState(null);
+	const [hasFocus, setHasFocus] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+	const [selected, setSelected] = useState(defaultOptionIndex);
+	const [actionName, setActionName] = useState(get([selected, 'title'], options));
 
-    const labelId = id.replace('dropdown-button-', 'dropdown-button-label-');
-    const listId = id.replace('dropdown-button-', 'dropdown-button-list-');
+	const labelId = id.replace('dropdown-button-', 'dropdown-button-label-');
+	const listId = id.replace('dropdown-button-', 'dropdown-button-list-');
 
-    const handleKeyDown = (e) => {
+	const handleKeyDown = (e) => {
 
-        let index = active;
-        let increment = true;
+		let index = active;
+		let increment = true;
 
-        const ESCAPE = 27;
-        const SPACE = 32;
-        const UP_ARROW = 38;
-        const DOWN_ARROW = 40;
+		const ESCAPE = 27;
+		const SPACE = 32;
+		const UP_ARROW = 38;
+		const DOWN_ARROW = 40;
 
-        switch (e.keyCode) {
-            case ESCAPE:
-                toggle.current.blur();
-                setIsOpen(false);
-                return;
-            case SPACE:
-                e.preventDefault();
-                setSelected(active);
-                return;
-            case UP_ARROW:
-                e.preventDefault();
-                index--;
-                increment = false;
-                break;
-            case DOWN_ARROW:
-                e.preventDefault();
-                index++;
-                break;
-        }
+		switch (e.keyCode) {
+			case ESCAPE:
+				toggle.current.blur();
+				setIsOpen(false);
+				return;
+			case SPACE:
+				e.preventDefault();
+				setSelected(active);
+				return;
+			case UP_ARROW:
+				e.preventDefault();
+				index--;
+				increment = false;
+				break;
+			case DOWN_ARROW:
+				e.preventDefault();
+				index++;
+				break;
+		}
 
-        while (index < 0 || index >= options.length || index === selected) {
+		while (index < 0 || index >= options.length || index === selected) {
 
-            if (index < 0) {
-                index = options.length - 1;
-            }
+			if (index < 0) {
+				index = options.length - 1;
+			}
 
-            if (index >= options.length) {
-                index = 0;
-            }
+			if (index >= options.length) {
+				index = 0;
+			}
 
-            if (index === selected) {
-                increment ? index++ : index--;
-            }
+			if (index === selected) {
+				increment ? index++ : index--;
+			}
 
-        }
+		}
 
-        setActive(index);
+		setActive(index);
 
-    };
+	};
 
-    useEffect(() => {
-        setActiveDescendant(`${id}-${get([active, 'label'], options)}`);
-    }, [active]);
+	useEffect(() => {
+		setActiveDescendant(`${id}-${get([active, 'label'], options)}`);
+	}, [active]);
 
-    useEffect(() => {
-        setIsOpen(false);
-        setActionName(get([selected, 'title'], options));
-        if (hasFocus && triggerOnSelect) {
-            const callback = get([selected, 'callback'], options);
-            if (typeof callback === 'function') {
-                callback(options[selected]);
-            }
-        }
-    }, [selected]);
+	useEffect(() => {
+		setActionName(get([selected, 'title'], options));
+		if (hasFocus && triggerOnSelect) {
+			const callback = get([selected, 'callback'], options);
+			if (typeof callback === 'function') {
+				callback(options[selected]);
+			}
+		}
+	}, [selected]);
 
-    useEffect(() => {
-        if (!hasFocus) {
-            setIsOpen(false);
-        }
-    }, [hasFocus]);
+	useEffect(() => {
+		if (!hasFocus) {
+			setIsOpen(false);
+		}
+	}, [hasFocus]);
 
-    useEffect(() => {
-        if (!isOpen) {
-            setActive(0);
-        }
-    }, [isOpen]);
+	useEffect(() => {
+		if (!isOpen) {
+			setActive(0);
+		}
+	}, [isOpen]);
 
-    return (
-        <div
-            className={classNames({
-                'dropdown-button': true,
-                '--is-open': isOpen,
-                [className]: className.length,
-            })}
-            ref={component}
-        >
-            <div className="dropdown-button__label" id={labelId}>{label}</div>
-            <div className="dropdown-button__button-group">
-                <Button
-                    className="dropdown-button__action"
-                    disabled={disabled}
-                    isDefault
-                    onClick={() => {
-                        const callback = get([selected, 'callback'], options);
-                        if (typeof callback === 'function') {
-                            callback(options[selected]);
-                        }
-                    }}
-                >
-                    {actionName}
-                </Button>
-                <Button
-                    aria-expanded={isOpen ? 'true' : 'false'}
-                    aria-haspopup="listbox"
-                    aria-labelledby={labelId}
-                    className="dropdown-button__toggle components-button bluehost is-button is-default"
-                    disabled={disabled}
-                    onClick={() => setIsOpen(!isOpen)}
-                    onFocus={() => setHasFocus(true)}
-                    onBlur={() => setHasFocus(false)}
-                    onKeyDown={handleKeyDown}
-                    ref={toggle}
-                >
-                    <DownIcon/>
-                    <span className="sr-only">{__('Select', 'bluehost-wordpress-plugin')}</span>
-                </Button>
-            </div>
-            <ul
-                aria-activedescendant={activeDescendant}
-                aria-hidden={!isOpen}
-                className={classNames({
-                    'dropdown-button__list': true,
-                    '--is-visible': isOpen,
-                })}
-                id={listId}
-                role="listbox"
-                style={{width}}
-                tabIndex="-1"
-            >
-                {options.map(({callback: itemCallback, title: itemTitle, description: itemDescription}, index) => {
-                    const itemId = `${id.replace('dropdown-button-', 'dropdown-button-list-item-')}-${kebabCase(itemTitle)}`;
-                    return (
-                        <li
-                            aria-selected={index === active ? 'true' : 'false'}
-                            className={classNames({
-                                'dropdown-button__list-item': true,
-                                '--is-active': index === active,
-                            })}
-                            id={itemId}
-                            key={index}
-                            onMouseDown={() => setSelected(index)}
-                            onMouseEnter={() => setActive(index)}
-                            onTouchStart={() => setSelected(index)}
-                            role="option"
-                        >
-                            <div className="dropdown-button__list-item-title">{itemTitle}</div>
-                            <div className="dropdown-button__list-item-description">{itemDescription}</div>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+	const triggerCallback = (selected) => {
+		const callback = get([selected, 'callback'], options);
+		if (typeof callback === 'function') {
+			callback(options[selected]);
+		}
+	};
+
+	const maybeTriggerCallback = (index) => {
+		if (triggerOnSelect && selected === index) {
+			triggerCallback(selected);
+		}
+	};
+
+	return (
+		<div
+			className={classNames({
+				'dropdown-button': true,
+				'--is-open': isOpen,
+				[className]: className.length,
+			})}
+			ref={component}
+		>
+			<div className="dropdown-button__label" id={labelId}>{label}</div>
+			<div className="dropdown-button__button-group">
+				<Button
+					className="dropdown-button__action"
+					disabled={disabled}
+					isDefault
+					onClick={() => {
+						const callback = get([selected, 'callback'], options);
+						if (typeof callback === 'function') {
+							callback(options[selected]);
+						}
+					}}
+				>
+					{actionName}
+				</Button>
+				<Button
+					aria-expanded={isOpen ? 'true' : 'false'}
+					aria-haspopup="listbox"
+					aria-labelledby={labelId}
+					className="dropdown-button__toggle components-button bluehost is-button is-default"
+					disabled={disabled}
+					onClick={() => setIsOpen(!isOpen)}
+					onFocus={() => setHasFocus(true)}
+					onBlur={() => setHasFocus(false)}
+					onKeyDown={handleKeyDown}
+					ref={toggle}
+				>
+					<DownIcon/>
+					<span className="sr-only">{__('Select', 'bluehost-wordpress-plugin')}</span>
+				</Button>
+			</div>
+			<ul
+				aria-activedescendant={activeDescendant}
+				aria-hidden={!isOpen}
+				className={classNames({
+					'dropdown-button__list': true,
+					'--is-visible': isOpen,
+				})}
+				id={listId}
+				role="listbox"
+				style={{width}}
+				tabIndex="-1"
+			>
+				{options.map(({callback: itemCallback, title: itemTitle, description: itemDescription}, index) => {
+					const itemId = `${id.replace('dropdown-button-', 'dropdown-button-list-item-')}-${kebabCase(itemTitle)}`;
+					return (
+						<li
+							aria-selected={index === active ? 'true' : 'false'}
+							className={classNames({
+								'dropdown-button__list-item': true,
+								'--is-active': index === active,
+							})}
+							id={itemId}
+							key={index}
+							onMouseDown={() => {
+								setSelected(index);
+								maybeTriggerCallback(index);
+							}}
+							onMouseEnter={() => setActive(index)}
+							onTouchStart={() => {
+								setSelected(index);
+								maybeTriggerCallback(index);
+							}}
+							role="option"
+						>
+							<div className="dropdown-button__list-item-title">{itemTitle}</div>
+							<div className="dropdown-button__list-item-description">{itemDescription}</div>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
