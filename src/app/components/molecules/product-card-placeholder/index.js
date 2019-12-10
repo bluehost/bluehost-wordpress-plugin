8ef6f1151716eee7a2d91426525778368960465d
David Ryan <reachdavidryan@gmail.com>
2019-12-10 07:51:45 -0700

    remove moved/renamed folders

--- src/app/components/molecules/product-card-placeholder/index.js
+++
@@ -1,7 +0,0 @@
-import './style.scss';
-
-import { BWAProductCard } from './node_modules/@/components/molecules';
-
-export default function BWABWAProductCardPlaceholder() {
-	return <BWAProductCard className="--placeholder" />;
-}
