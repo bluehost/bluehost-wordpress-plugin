8a4ca764b7364237471617a49cf08b3a86aba5a6
Micah Wood <micah@wpscholar.com>
2020-04-07 16:54:32 -0400

    Added GitHub actions.

---
+++ .github/scripts/cleanup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -rf .git
+rm -rf .github
+rm -rf node_modules
+rm -rf src
+rm -rf tests
+find . -name '.*' -type f -maxdepth 1 -delete
+find . -name '*.md' -type f -maxdepth 1 -delete
+find . -name '*.json' -type f -maxdepth 1 -delete
+find . -name '*.lock' -type f -maxdepth 1 -delete
+find . -name '*.xml' -type f -maxdepth 1 -delete
+rm webpack.config.js
