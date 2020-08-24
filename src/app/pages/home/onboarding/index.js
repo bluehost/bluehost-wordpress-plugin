f5140375d6b08fc9cb3215995fc1da0844e10f37
Micah Wood <micah@wpscholar.com>
2020-08-24 14:00:31 -0400

    Finish layout of onboarding page

--- src/app/pages/home/onboarding/index.js
+++ src/app/pages/home/onboarding/index.js
@@ -2,11 +2,16 @@
  * WordPress dependencies
  */
 import { useSelect } from '@wordpress/data';
+import { __, sprintf } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import { BWAButton as Button } from '@app/components/atoms';
+import comment from '@app/assets/comment.png';
+import computer from '@app/assets/computer.png';
+import rocket from '@app/assets/rocket.png';
+import './style.scss';
 
 export default function Onboarding() {
 
@@ -24,48 +29,85 @@ export default function Onboarding() {
 
 			<header className="bluehost-onboarding-page-header">
 				<h2 className="bluehost-onboarding-page-header__title">
-					Welcome to your WordPress site
+					{ __('Welcome to your WordPress site', 'bluehost-wordpress-plugin') }
 				</h2>
 				<p className="bluehost-onboarding-page-header__description">
-					Not sure how to get started? Here are a few options we recommend.
+					{ __('Not sure how to get started? Here are a few options we recommend.', 'bluehost-wordpress-plugin') }
 				</p>
 			</header>
 
 			<div className="bluehost-onboarding-page-content">
 
-				<div className="bluhost-onboarding-steps">
+				<div className="bluehost-onboarding-steps">
 
-					<div className="bluhost-onboarding-step">
-						<div>Start with a page or post</div>
-						<p>
-							Use a page for content that won&apos;t change too often (your homepage, portfolio), and use posts for
-							information that&apos;s timely (blog posts, updates).
+					<div className="bluehost-onboarding-step">
+						<div className="bluehost-onboarding-step__image">
+							<img src={ comment } alt="" />
+						</div>
+						<div className="bluehost-onboarding-step__title">
+							{ __('Start with a page or post', 'bluehost-wordpress-plugin') }
+						</div>
+						<p className="bluehost-onboarding-step__description">
+							{ __("Use a page for content that won't change too often (your homepage, portfolio), and use posts for information that's timely (blog posts, updates).", 'bluehost-wordpress-plugin') }
 						</p>
-						<Button href={ `${ baseUrl }post-new.php` } isLink>Add a blog post</Button>
-						<Button href={ `${ baseUrl }post-new.php?post_type=page` } isPrimary>Add a page</Button>
+						<div className="bluehost-onboarding-step__actions">
+							<Button href={ `${ baseUrl }post-new.php` } isLink>
+								{ __('Add a blog post', 'bluehost-wordpress-plugin') }
+							</Button>
+							<Button href={ `${ baseUrl }post-new.php?post_type=page` } isSecondary>
+								{ __('Add a page', 'bluehost-wordpress-plugin') }
+							</Button>
+						</div>
 					</div>
 
-					<div className="bluhost-onboarding-step">
-						<div>Make it look just right</div>
-						<p>
-							Use the customizer to go simple, bold&emdash;whatever&apos;s you.<br />
-							Or browse themes and try &apos;em out.
+					<div className="bluehost-onboarding-step">
+						<div className="bluehost-onboarding-step__image">
+							<img src={ computer } alt="" />
+						</div>
+						<div className="bluehost-onboarding-step__title">
+							{ __('Make it look just right', 'bluehost-wordpress-plugin') }
+						</div>
+						<p className="bluehost-onboarding-step__description">
+							{ __("Use the customizer to go simple, bold—whatever's you.", 'bluehost-wordpress-plugin') }<br />
+							{ __("Or browse themes and try 'em out.", 'bluehost-wordpress-plugin') }
 						</p>
-						<Button href={ `${ baseUrl }admin.php?page=bluehost#/marketplace/themes` } isLink>Browse themes</Button>
-						<Button href={ `${ baseUrl }customize.php` } isPrimary>Customize your site</Button>
+						<div className="bluehost-onboarding-step__actions">
+							<Button href={ `${ baseUrl }admin.php?page=bluehost#/marketplace/themes` } isLink>
+								{ __('Browse themes', 'bluehost-wordpress-plugin') }
+							</Button>
+							<Button href={ `${ baseUrl }customize.php` } isSecondary>
+								{ __('Customize your site', 'bluehost-wordpress-plugin') }
+							</Button>
+						</div>
 					</div>
 
-					<div className="bluhost-onboarding-step">
-						<div>Get ready to launch</div>
-						<p>
-							Your site has a &ldquo;coming soon&rdquo; message, which lets people know you&apos;re working on it.
-							Check out our&nbsp;
-							<a href="https://www.bluehost.com/help/article/website-publish-checklist">
-								website pre-publishing checklist
-							</a>&nbsp;
-							to help get ready for launch.
+					<div className="bluehost-onboarding-step">
+						<div className="bluehost-onboarding-step__image">
+							<img src={ rocket } alt="" />
+						</div>
+						<div className="bluehost-onboarding-step__title">
+							{ __('Get ready to launch', 'bluehost-wordpress-plugin') }
+						</div>
+						<p className="bluehost-onboarding-step__description">
+							{ __('Your site has a \"coming soon\" message, which lets people know you\'re working on it. ', 'bluehost-wordpress-plugin') }
+							<span
+								dangerouslySetInnerHTML={
+									{
+										__html: sprintf(
+											__('Check out our %swebsite pre-publishing%s checklist to help get ready for launch.', 'bluehost-wordpress-plugin'),
+											'<a href="https://www.bluehost.com/help/article/website-publish-checklist">',
+											'</a>'
+										)
+									}
+								}
+							/>
+
 						</p>
-						<Button onClick={ launchHandler } isPrimary>Launch your site</Button>
+						<div className="bluehost-onboarding-step__actions">
+							<Button onClick={ launchHandler } isPrimary>
+								{ __('Launch your site', 'bluehost-wordpress-plugin') }
+							</Button>
+						</div>
 					</div>
 
 				</div>
@@ -75,23 +117,38 @@ export default function Onboarding() {
 			<footer className="bluehost-onboarding-page-footer">
 
 				<h3 className="bluehost-onboarding-page-footer__title">
-					How-to&apos;s & next steps
+					{ __("How-to's & next steps", 'bluehost-wordpress-plugin') }
 				</h3>
 
 				<div className="bluehost-onboarding-page-footer__content">
 					<p>
-						Learn how to add functionality to your site with plugins:<br />
-						<a href="#">How to use WordPress plugins</a>
-						<a href="#">The two essential plugins you need</a>
+						{ __('Learn how to add functionality to your site with plugins:', 'bluehost-wordpress-plugin') }
+						<br />
+						<a href="https://www.bluehost.com/blog/how-to-use-wordpress-plugins/">
+							{ __('How to use WordPress plugins', 'bluehost-wordpress-plugin') }
+						</a>
+						<br />
+						<a href="https://www.bluehost.com/blog/the-two-essential-plugins-you-need-for-your-wordpress-site/">
+							{ __('The two essential plugins you need', 'bluehost-wordpress-plugin') }
+						</a>
 					</p>
 					<p>
-						Check out ecommerce tips:<br />
-						<a href="#">Steps for adding a store to your site with WooCommerce</a>
-						<a href="#">The five best WooCommerce WordPress themes</a>
+						{ __('Check out ecommerce tips:', 'bluehost-wordpress-plugin') }
+						<br />
+						<a href="https://www.bluehost.com/blog/how-to-use-woocommerce/">
+							{ __('Steps for adding a store to your site with WooCommerce', 'bluehost-wordpress-plugin') }
+						</a>
+						<br />
+						<a href="https://www.bluehost.com/blog/the-five-best-woocommerce-wordpress-themes/">
+							{ __('The five best WooCommerce WordPress themes', 'bluehost-wordpress-plugin') }
+						</a>
 					</p>
 					<p>
-						Get help from WordPress experts:<br />
-						<a href="#">Our BlueSky experts are here to help you every step of the way</a>
+						{ __('Get help from WordPress experts:', 'bluehost-wordpress-plugin') }
+						<br />
+						<a href="https://www.bluehost.com/blue-sky">
+							{ __('Our BlueSky experts are here to help you every step of the way', 'bluehost-wordpress-plugin') }
+						</a>
 					</p>
 				</div>
 
