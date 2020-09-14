f5598c1b739fd05f754bd3d82a1eaca6fd4d9d61
Micah Wood <micah@wpscholar.com>
2020-09-14 15:38:24 -0400

    Update plugin tests to be more robust

--- tests/cypress/integration/plugins.spec.js
+++ tests/cypress/integration/plugins.spec.js
@@ -1,40 +1,7 @@
 // <reference types="Cypress" />
 
-import 'cypress-axe';
-
 const plugins = require('../fixtures/plugins.json');
 const {_} = Cypress;
-
-const fn = {
-	decodeHtml(html) {
-		const textarea = document.createElement('textarea');
-		textarea.innerHTML = html;
-		return textarea.value;
-	},
-	getCard(index) {
-		const item = plugins.items[index];
-		return {
-			backgroundImageUrl: item.images.preview_url,
-			buttonText: 'View Details',
-			id: item.id,
-			linkText: 'Buy Now',
-			linkUrl: item.buy_url,
-			price: item.prices.single_domain_license,
-			title: item.name,
-		};
-	},
-	validateCard(selector, cardIndex) {
-		selector.within(() => {
-			cy.contains('.product-card__title', this.decodeHtml(this.getCard(cardIndex).title));
-		});
-	},
-	validateCards(collection = [...Array(12).keys()]) {
-		collection.forEach((item, index) => {
-			let cardIndex = typeof item === 'number' ? item : _.indexOf(plugins.items, _.find(plugins.items, {name: item}));
-			this.validateCard(cy.get('.product-card').eq(index), cardIndex);
-		})
-	}
-};
 
 describe('Plugins Page', function () {
 
@@ -51,27 +18,34 @@ describe('Plugins Page', function () {
 	});
 
 	it('Is Accessible', () => {
-		cy.wait(1000);
+		cy.wait(100);
 		cy.checkA11y('.router-section');
 	});
 
-	it('BWAProductGrid has 12 items', () => {
+	it('Product grid has 12 items', () => {
 		cy.get('.product-card').should('have.length', 12);
 	});
 
 	it('Product cards render correctly', () => {
-		cy.get('.product-card').first().should('be.visible');
-		cy.get('.product-card').first().within(() => {
-			const {backgroundImageUrl, buttonText, linkText, linkUrl, price, title} = fn.getCard(0);
+		cy.get('.product-card').as('card');
+
+		cy.get('@card')
+			.findByRole('button', {name: 'View Details'})
+			.scrollIntoView()
+			.should('be.visible');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Buy Now'})
+			.should('have.attr', 'href')
+			.and('include', 'https://www.mojomarketplace.com/cart?item_id=');
+
+		cy.get('@card').first().within(() => {
+			const {backgroundImageUrl, price, title} = getCard(0);
+			cy.root().scrollIntoView().should('be.visible');
 			cy.get('.product-card__image').should('be.visible');
 			cy.get('.product-card__image').should('have.css', 'background-image', `url("${ backgroundImageUrl }")`);
 			cy.contains('.product-card__title', title);
 			cy.contains('.product-card__price', price);
-			cy.get('.product-card__action-group').within(() => {
-				cy.contains('button', buttonText);
-				cy.contains('a', linkText);
-				cy.get('a').should('have.attr', 'href').and('include', linkUrl);
-			});
 		});
 	});
 
@@ -86,11 +60,6 @@ describe('Plugins Page', function () {
 	it('Search works', () => {
 		cy.get('input[type="search"]').type('WooCommerce{enter}');
 		cy.get('.product-card').should('have.length', 12);
-		fn.validateCards([
-			'Xero Integrate Your WooCommerce WordPress Plugin',
-			'WooCommerce Bookings Developed by WooCommerce',
-			'SW Product Bundles WooCommerce Bundled Product WordPress Plugin',
-		]);
 	});
 
 	it('Search with no results works', () => {
@@ -112,32 +81,19 @@ describe('Plugins Page', function () {
 		});
 	});
 
-	it('Sort by Popular', () => {
-		// Select a different option first so React will update when we filter by popular
-		cy.get('.dropdown__item').eq(1).click({force: true});
-		cy.wait(100);
-		cy.get('.dropdown__item').eq(0).click({force: true}); // Popular
-		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
-	});
-
 	it('Sort by Price (High to Low)', () => {
 		cy.get('.dropdown__item').eq(1).click({force: true}); // Price (High to Low)
 		cy.validateProductCardOrder('WooCommerce Bookings Developed by WooCommerce', 0);
-		cy.get('.pagination').first().find('[aria-label="Last"]').click();
-		cy.validateProductCardOrder('Image Gallery WordPress Image Gallery', 0);
-	});
-
-	it('Sort by Price (Low to High)', () => {
-		cy.get('.dropdown__item').eq(2).click({force: true}); // Price (Low to High)
-		cy.get('.product-card__price').eq(0).should;
-		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
-		cy.get('.pagination').first().find('[aria-label="Last"]').click();
-		cy.validateProductCardOrder('UPS Shipping Method for WooCommerce', 0);
 	});
 
 	it('Sort by Date Added', () => {
 		cy.get('.dropdown__item').eq(3).click({force: true}); // Date Added
 		cy.validateProductCardOrder('Reinvestment WP Reinvestment Calculator for WordPress', 0);
+	});
+
+	it('Sort by Price (Low to High)', () => {
+		cy.get('.dropdown__item').eq(2).click({force: true}); // Price (Low to High)
+		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
 	});
 
 	it.skip('Filter by favorites when no favorites are set', () => {
@@ -157,40 +113,62 @@ describe('Plugins Page', function () {
 		cy.get('.dropdown__item').eq(4).click({force: true}); // Favorites
 		cy.get('.pagination').should('have.length', 0);
 		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
-		cy.validateProductCardOrder('Product Vendors Plugin for WordPress', 1);
-		cy.validateProductCardOrder('Gwolle GB Add-On Adds Functions to Your Guestbook', 2);
 	});
 
-	it.skip('Pagination exists', () => {
-		// Reset so subsequent tests don't fail
+	it('Sort by Popular', () => {
 		cy.get('.dropdown__item').eq(0).click({force: true}); // Popular
+		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
+	});
 
+	it('Pagination exists', () => {
 		cy.get('.pagination').should('have.length', 2);
 	});
 
-	it.skip('Pagination: Numeric navigation works', () => {
-		cy.get('.pagination > .pagination__item').eq(1).find('button').click();
+	it('Pagination: Numeric navigation works', () => {
+		cy.get('.pagination').findByRole('button', {name: '2'}).click();
 		cy.validateProductCardOrder('Social Share Wordpress Social Media', 0);
 	});
 
-	it.skip('Pagination: Previous button works', () => {
-		cy.get('.pagination').first().find('[aria-label="Previous"]').click();
+	it('Pagination: Previous button works', () => {
+		cy.get('.pagination').findByLabelText('Previous').click();
 		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
 	});
 
-	it.skip('Pagination: Next button works', () => {
-		cy.get('.pagination').first().find('[aria-label="Next"]').click();
+	it('Pagination: Next button works', () => {
+		cy.get('.pagination').findByLabelText('Next').click();
 		cy.validateProductCardOrder('Product Add-Ons Developed by WooCommerce', 0);
 	});
 
-	it.skip('Pagination: First button works', () => {
-		cy.get('.pagination').first().find('[aria-label="First"]').click();
+	it('Pagination: First button works', () => {
+		cy.get('.pagination').findByLabelText('First').click();
 		cy.validateProductCardOrder('Constant Contact Forms For WordPress', 0);
 	});
 
-	it.skip('Pagination: Last button works', () => {
-		cy.get('.pagination').first().find('[aria-label="Last"]').click();
+	it('Pagination: Last button works', () => {
+		cy.get('.pagination').findByLabelText('Last').click();
 		cy.validateProductCardOrder('Responsive Gallery WordPress BWAProductGrid Gallery', 0);
 	});
 
 });
+
+//////////////////////
+// Helper Functions //
+//////////////////////
+
+/**
+ * Get a product card by index.
+ *
+ * @returns {object} The card details.
+ */
+function getCard(index) {
+	const item = plugins.items[index];
+	return {
+		backgroundImageUrl: item.images.preview_url,
+		buttonText: 'View Details',
+		id: item.id,
+		linkText: 'Buy Now',
+		linkUrl: item.buy_url,
+		price: item.prices.single_domain_license,
+		title: item.name,
+	};
+}
