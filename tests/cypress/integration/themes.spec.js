e8103648392925f48f7fc4f4c1d601d157b0dd67
Micah Wood <micah@wpscholar.com>
2020-09-14 15:48:36 -0400

    Clean up themes tests

--- tests/cypress/integration/themes.spec.js
+++ tests/cypress/integration/themes.spec.js
@@ -1,41 +1,7 @@
 // <reference types="Cypress" />
 
-import 'cypress-axe';
-
 const themes = require('../fixtures/themes.json');
 const {_} = Cypress;
-
-const fn = {
-	decodeHtml(html) {
-		const textarea = document.createElement('textarea');
-		textarea.innerHTML = html;
-		return textarea.value;
-	},
-	getCard(index) {
-		const item = themes.items[index];
-		return {
-			backgroundImageUrl: item.images.preview_url,
-			id: item.id,
-			defaultLinkText: 'Preview',
-			defaultLinkUrl: `/wp-admin/admin.php?page=mojo-theme-preview&id=${ item.id }`,
-			price: item.prices.single_domain_license,
-			primaryLinkText: 'Buy Now',
-			primaryLinkUrl: item.buy_url,
-			title: item.name,
-		};
-	},
-	validateCard(selector, cardIndex) {
-		selector.within(() => {
-			cy.contains('.product-card__title', this.decodeHtml(this.getCard(cardIndex).title));
-		});
-	},
-	validateCards(collection = [...Array(12).keys()]) {
-		collection.forEach((item, index) => {
-			let cardIndex = typeof item === 'number' ? item : _.indexOf(themes.items, _.find(themes.items, {name: item}));
-			this.validateCard(cy.get('.product-card').eq(index), cardIndex);
-		})
-	}
-};
 
 describe('Themes Page', function () {
 
@@ -52,35 +18,34 @@ describe('Themes Page', function () {
 	});
 
 	it('Is Accessible', () => {
+		cy.wait(100);
 		cy.checkA11y('.router-section');
 	});
 
-	it('BWAProductGrid has 12 items', () => {
+	it('Product grid has 12 items', () => {
 		cy.get('.product-card').should('have.length', 12);
 	});
 
 	it('Product cards render correctly', () => {
-		cy.get('.product-card').first().should('be.visible');
-		cy.get('.product-card').first().within(() => {
-			const {
-				backgroundImageUrl,
-				defaultLinkText,
-				defaultLinkUrl,
-				price,
-				primaryLinkText,
-				primaryLinkUrl,
-				title
-			} = fn.getCard(0);
+		cy.get('.product-card').as('card');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Preview'})
+			.should('have.attr', 'href')
+			.and('include', '/wp-admin/admin.php?page=mojo-theme-preview&id=');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Buy Now'})
+			.should('have.attr', 'href')
+			.and('include', 'https://www.mojomarketplace.com/cart?item_id=');
+
+		cy.get('@card').first().within(() => {
+			const {backgroundImageUrl, price, title} = getCard(0);
+			cy.root().scrollIntoView().should('be.visible');
 			cy.get('.product-card__image').should('be.visible');
 			cy.get('.product-card__image').should('have.css', 'background-image', `url("${ backgroundImageUrl }")`);
 			cy.contains('.product-card__title', title);
 			cy.contains('.product-card__price', price);
-			cy.get('.product-card__action-group').within(() => {
-				cy.get('a').first().contains(defaultLinkText);
-				cy.get('a').first().should('have.attr', 'href').and('include', defaultLinkUrl);
-				cy.get('a').eq(1).contains(primaryLinkText);
-				cy.get('a').eq(1).should('have.attr', 'href').and('include', primaryLinkUrl);
-			});
 		});
 	});
 
@@ -95,11 +60,6 @@ describe('Themes Page', function () {
 	it('Search works', () => {
 		cy.get('input[type="search"]').type('WooCommerce{enter}');
 		cy.get('.product-card').should('have.length', 12);
-		fn.validateCards([
-			'Nectar A Responsive WordPress Theme',
-			'Everline Wedding WordPress Theme',
-			'Henry WooCommerce WordPress Theme',
-		]);
 	});
 
 	it('Search with no results works', () => {
@@ -110,7 +70,7 @@ describe('Themes Page', function () {
 
 	it('Clearing search works', () => {
 		cy.get('input[type="search"]').clear().type('{enter}');
-		cy.validateProductCardOrder('Highend Ultimate Multi-Purpose WordPress Theme', 0);
+		cy.get('.product-card').should('have.length', 12);
 	});
 
 	it('Sort field exists', () => {
@@ -126,15 +86,14 @@ describe('Themes Page', function () {
 		cy.validateProductCardOrder('The Best Theme for LearnDash', 0);
 	});
 
-	it('Sort by Price (Low to High)', () => {
-		cy.get('.dropdown__item').eq(2).click({force: true}); // Price (Low to High)
-		cy.get('.product-card__price').eq(0).should;
-		cy.validateProductCardOrder('TickTock – Under Construction/Coming soon WordPress Theme', 0);
-	});
-
 	it('Sort by Date Added', () => {
 		cy.get('.dropdown__item').eq(3).click({force: true}); // Date Added
 		cy.validateProductCardOrder('Insura Business WordPress Theme', 0);
+	});
+
+	it('Sort by Price (Low to High)', () => {
+		cy.get('.dropdown__item').eq(2).click({force: true}); // Price (Low to High)
+		cy.validateProductCardOrder('TickTock – Under Construction/Coming soon WordPress Theme', 0);
 	});
 
 	it.skip('Filter by favorites when no favorites are set', () => {
@@ -153,14 +112,10 @@ describe('Themes Page', function () {
 
 		cy.get('.dropdown__item').eq(4).click({force: true}); // Favorites
 		cy.get('.pagination').should('have.length', 0);
-		fn.validateCards([
-			'Highend Ultimate Multi-Purpose WordPress Theme',
-			'Daisy Blog WordPress Theme',
-			'Birch A Responsive WordPress Theme',
-		]);
+		cy.validateProductCardOrder('Highend Ultimate Multi-Purpose WordPress Theme', 0);
 	});
 
-	it.skip('Sort by Popular', () => {
+	it('Sort by Popular', () => {
 		cy.get('.dropdown__item').eq(0).click({force: true}); // Popular
 		cy.validateProductCardOrder('Highend Ultimate Multi-Purpose WordPress Theme', 0);
 	});
@@ -169,29 +124,51 @@ describe('Themes Page', function () {
 		cy.get('.pagination').should('have.length', 2);
 	});
 
-	it.skip('Pagination: Numeric navigation works', () => {
-		cy.get('.pagination > .pagination__item').eq(1).find('button').click();
-		fn.validateCards(['Creatica Creative Multi-Purpose WordPress Theme']);
+	it('Pagination: Numeric navigation works', () => {
+		cy.get('.pagination').findByRole('button', {name: '2'}).click();
+		cy.validateProductCardOrder('Creatica Creative Multi-Purpose WordPress Theme', 0);
 	});
 
-	it.skip('Pagination: Previous button works', () => {
-		cy.get('.pagination').first().find('[aria-label="Previous"]').click();
-		fn.validateCards(['Highend Ultimate Multi-Purpose WordPress Theme']);
+	it('Pagination: Previous button works', () => {
+		cy.get('.pagination').findByLabelText('Previous').click();
+		cy.validateProductCardOrder('Highend Ultimate Multi-Purpose WordPress Theme', 0);
 	});
 
-	it.skip('Pagination: Next button works', () => {
-		cy.get('.pagination').first().find('[aria-label="Next"]').click();
-		fn.validateCards(['Creatica Creative Multi-Purpose WordPress Theme']);
+	it('Pagination: Next button works', () => {
+		cy.get('.pagination').findByLabelText('Next').click();
+		cy.validateProductCardOrder('Creatica Creative Multi-Purpose WordPress Theme', 0);
 	});
 
-	it.skip('Pagination: First button works', () => {
-		cy.get('.pagination').first().find('[aria-label="First"]').click();
-		fn.validateCards(['Highend Ultimate Multi-Purpose WordPress Theme']);
+	it('Pagination: First button works', () => {
+		cy.get('.pagination').findByLabelText('First').click();
+		cy.validateProductCardOrder('Highend Ultimate Multi-Purpose WordPress Theme', 0);
 	});
 
-	it.skip('Pagination: Last button works', () => {
-		cy.get('.pagination').first().find('[aria-label="Last"]').click();
-		fn.validateCards(['TreeBeard Nature WordPress Theme']);
+	it('Pagination: Last button works', () => {
+		cy.get('.pagination').findByLabelText('Last').click();
+		cy.validateProductCardOrder('TreeBeard Nature WordPress Theme', 0);
 	});
 
 });
+
+//////////////////////
+// Helper Functions //
+//////////////////////
+
+/**
+ * Get a product card by index.
+ *
+ * @returns {object} The card details.
+ */
+function getCard(index) {
+	const item = themes.items[index];
+	return {
+		backgroundImageUrl: item.images.preview_url,
+		buttonText: 'View Details',
+		id: item.id,
+		linkText: 'Buy Now',
+		linkUrl: item.buy_url,
+		price: item.prices.single_domain_license,
+		title: item.name,
+	};
+}
