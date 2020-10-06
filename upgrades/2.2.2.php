3459758b4b67c5e8fb022d6a10c1ce261162d6fd
Micah Wood <micah@wpscholar.com>
2020-10-06 15:39:44 -0400

    fix(Upgrade) - Remove old cron schedules from previous plugin refactoring

--- upgrades/2.2.2.php
+++ upgrades/2.2.2.php
@@ -10,3 +10,9 @@ if ( is_plugin_active( 'mojo-marketplace-wp-plugin/mojo-marketplace.php' ) ) {
 	deactivate_plugins( 'mojo-marketplace-wp-plugin/mojo-marketplace.php' );
 }
 
+// Remove old cron schedules
+wp_clear_scheduled_hook( 'mm_cron_hourly' );
+wp_clear_scheduled_hook( 'mm_cron_twicedaily' );
+wp_clear_scheduled_hook( 'mm_cron_daily' );
+wp_clear_scheduled_hook( 'mm_cron_weekly' );
+wp_clear_scheduled_hook( 'mm_cron_monthly' );
