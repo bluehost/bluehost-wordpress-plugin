1915b6dd21490ff17a541d4511b08e08b4b639ae
Mike Hansen <mike.hansen@endurance.com>
2020-10-08 16:17:44 -0600

    Add redirect for jetpack with back-compat with mojo.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -33,3 +33,15 @@ function bluehost_jetpack_unregister_mailchimp_block( $blocks ) {
 }
 
 add_filter( 'jetpack_set_available_blocks', 'bluehost_jetpack_unregister_mailchimp_block' );
+
+function bluehost_jetpack_connection_redirect() {
+	if ( isset( $_GET['page'] ) && in_array( $_GET['page'], array( 'mojo-jetpack-connect-bounce', 'bluehost-jetpack-connect-bounce' ) ) ) {
+		if ( class_exists( 'Jetpack' ) ) {
+			wp_redirect( Jetpack::init()->build_connect_url( true ), 302 );
+		} else {
+			// In the future this should be a Jetpack product page.
+			wp_safe_redirect( admin_url( 'admin.php?page=bluehost' ), 301 );
+		}
+	}
+}
+add_action( 'admin_init', 'bluehost_jetpack_connection_redirect' );
