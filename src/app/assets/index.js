1021037a30110bc23ba9378b718ee1d2bad753cf
Micah Wood <micah@wpscholar.com>
2020-10-30 13:27:21 -0400

    Add SVG assets

--- src/app/assets/index.js
+++ src/app/assets/index.js
@@ -1,6 +1,8 @@
 /**
  * External dependencies
  */
+import { ReactComponent as Analytics } from '@app/assets/svg/analytics.svg';
+import { ReactComponent as Bathtub } from '@app/assets/svg/bathtub.svg';
 import { ReactComponent as BillingIcon } from '@app/assets/svg/billing.svg';
 import { ReactComponent as BluehostLogo } from '@app/assets/svg/bluehost.svg';
 import { ReactComponent as BluerockSpinner } from '@app/assets/spinner.svg';
@@ -12,21 +14,36 @@ import { ReactComponent as ChatIcon } from '@app/assets/svg/chat.svg';
 import { ReactComponent as CloseIcon } from '@app/assets/svg/close.svg';
 import { ReactComponent as Computer } from '@app/assets/svg/computer.svg';
 import { ReactComponent as DownIcon } from '@app/assets/svg/down.svg';
+import { ReactComponent as Headset } from '@app/assets/svg/headset.svg';
 import { ReactComponent as HelpIcon } from '@app/assets/svg/help.svg';
 import { ReactComponent as JetpackLogo } from '@app/assets/svg/jetpack.svg';
+import { ReactComponent as Key } from '@app/assets/svg/key.svg';
+import { ReactComponent as MagnifyingGlass } from '@app/assets/svg/magnifying-glass.svg';
 import { ReactComponent as MenuIcon } from '@app/assets/svg/menu.svg';
+import { ReactComponent as Multimedia } from '@app/assets/svg/multimedia.svg';
+import { ReactComponent as Paint } from '@app/assets/svg/paint.svg';
+import { ReactComponent as PaperPlane } from '@app/assets/svg/paper-plane.svg';
 import { ReactComponent as ProductsIcon } from '@app/assets/svg/products.svg';
 import { ReactComponent as RadioButtonDisabledIcon } from '@app/assets/svg/radio-button-disabled.svg';
 import { ReactComponent as RadioButtonSelectedIcon } from '@app/assets/svg/radio-button-selected.svg';
 import { ReactComponent as RadioButtonUnselectedIcon } from '@app/assets/svg/radio-button-unselected.svg';
+import { ReactComponent as Responsive } from '@app/assets/svg/responsive.svg';
+import { ReactComponent as Rocket } from '@app/assets/svg/rocket.svg';
+import { ReactComponent as Screenshare } from '@app/assets/svg/screenshare.svg';
 import { ReactComponent as SearchIcon } from '@app/assets/search.svg';
 import { ReactComponent as SecurityIcon } from '@app/assets/svg/security.svg';
 import { ReactComponent as SEO } from '@app/assets/svg/seo.svg';
+import { ReactComponent as Shop } from '@app/assets/svg/shop.svg';
+import { ReactComponent as SocialShare } from '@app/assets/svg/social-share.svg';
+import { ReactComponent as Telescope } from '@app/assets/svg/telescope.svg';
+import { ReactComponent as Tractor } from '@app/assets/svg/tractor.svg';
 import { ReactComponent as UserIcon } from '@app/assets/svg/user.svg';
 import { ReactComponent as ValidationIcon } from '@app/assets/svg/validation.svg';
 import WorkingImageUrl, { ReactComponent as WorkingImage } from '@app/assets/svg/working.svg';
 
 export {
+	Analytics,
+	Bathtub,
 	BillingIcon,
 	BluehostLogo,
 	BluerockSpinner,
@@ -38,16 +55,29 @@ export {
 	CloseIcon,
 	Computer,
 	DownIcon,
+	Headset,
 	HelpIcon,
 	JetpackLogo,
+	Key,
+	MagnifyingGlass,
 	MenuIcon,
+	Multimedia,
+	Paint,
+	PaperPlane,
 	ProductsIcon,
 	RadioButtonDisabledIcon,
 	RadioButtonSelectedIcon,
 	RadioButtonUnselectedIcon,
+	Responsive,
+	Rocket,
+	Screenshare,
 	SearchIcon,
 	SecurityIcon,
 	SEO,
+	Shop,
+	SocialShare,
+	Telescope,
+	Tractor,
 	UserIcon,
 	ValidationIcon,
 	WorkingImage,
