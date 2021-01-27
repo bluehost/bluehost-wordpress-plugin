605ba98f8efe7803d70f13d31695205815199354
Micah Wood <micah@wpscholar.com>
2021-01-27 17:30:16 -0500

    Test moving code to before for ignoring uncaught exceptions

--- tests/cypress/integration/theme-preview.spec.js
+++ tests/cypress/integration/theme-preview.spec.js
@@ -3,12 +3,12 @@
 describe('Theme Preview Page', function () {
 
 	before(() => {
+		cy.on('uncaught:exception', () => false);
 		cy.server();
 		cy.visit('wp-admin/admin.php?page=mojo-theme-preview&id=5377b431-d8a8-431b-a711-50c10a141528');
 	});
 
 	it('Exists', () => {
-		cy.on('uncaught:exception', () => false);
 		cy.contains('.theme-name', 'Highend Ultimate Multipurpose WordPress Theme');
 		cy.get('iframe')
 			.should('have.attr', 'src')
