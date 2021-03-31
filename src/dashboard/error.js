01fc4aeb75fef705788e8e4d12ec1b936868305e
Dave Ryan <0aveRyan@users.noreply.github.com>
2021-03-31 09:47:55 -0700

    translatable error message in widget error

--- src/dashboard/error.js
+++ src/dashboard/error.js
@@ -1,7 +1,8 @@
+import { __ } from  '@wordpress/i18n';
 export const Error = () => {
     return (
         <div>
-            Error yo
+            <p>{__('Oops, something wen\'t wrong', 'bluehost-wordpress-plugin')}</p>
         </div>
     )
 }
