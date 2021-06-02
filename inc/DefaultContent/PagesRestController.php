1cc75ffccc9b4ed82fcca26431cbdabb8e844c4b
Dave Ryan <dave@bluehost.com>
2021-06-02 11:47:13 -0700

    update page contents from database

--- inc/DefaultContent/PagesRestController.php
+++ inc/DefaultContent/PagesRestController.php
@@ -51,62 +51,18 @@ class PagesRestController extends BaseHiiveController {
                 $title = 'Home';
                 // easy results from copy all block content
                 $content = <<<BLOCKCONTENT
-                <!-- wp:group {"className":"bh-instructions"} -->
-                <div class="wp-block-group bh-instructions"><div class="wp-block-group__inner-container"><!-- wp:paragraph -->
-                <p>Your website’s home page is your online storefront, billboard, and welcome mat. It’s where you make your first impression, so it’s important to get it right.</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p>This is the first thing you want your site visitors to see, so it should be engaging, helpful, and interesting. It could be as simple as the name of your company, or it might be tied to a promotion or sale that you’re running.</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p>Use this outline to fill in the content for your homepage:</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:list -->
-                <ul><li><strong>Header</strong><ul><li><strong>Hero image</strong> (this is the first thing your site visitors will see, so make it something good.)</li><li><strong>Headline</strong> (This is the most important thing you want people to read.)</li><li><strong>Subheadline</strong> or short paragraph (this is usually one or two sentences that give the headline context — it’s optional but useful and very common).</li><li><strong>Call to action</strong> — think of this as a shortcut to move visitors directly to the next step whether that’s products, promotions, or subscriptions.</li></ul></li></ul>
-                <!-- /wp:list -->
-
-                <!-- wp:paragraph -->
-                <p>What comes next will depend on the type of website you’re running.</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p>If your website is focused on selling a product or service, think about including:</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:list -->
-                <ul><li>“Reasons to believe” that help potential customers understand the value of your products.</li><li>Info on sales or promotions</li><li>Product listings and images</li><li>Testimonials or customer reviews</li></ul>
-                <!-- /wp:list -->
-
-                <!-- wp:paragraph -->
-                <p>If your website is focused on blogging or sharing other information, you might want to include:</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:list -->
-                <ul><li>Key information that supports your header</li><li>Most recent posts or news</li><li>Most popular blog posts</li><li>Top portfolio pieces</li></ul>
-                <!-- /wp:list -->
-
-                <!-- wp:paragraph -->
-                <p>Finish with a call to action (CTA).</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:paragraph -->
-                <p>This can be a button or link that directs people to their next steps. Depending on your site, this might be anything from contacting you, to setting up an appointment, looking at products, making a purchase, or subscribing to your blog or newsletter.</p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:image {"align":"center","width":600,"height":365,"sizeSlug":"large"} -->
+                <!-- wp:group {"className":"nf-instructions"} -->
+                <div class="wp-block-group nf-instructions"><div class="wp-block-group__inner-container"><!-- wp:image {"align":"center","width":600,"height":365,"sizeSlug":"large"} -->
                 <div class="wp-block-image"><figure class="aligncenter size-large is-resized"><img src="data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 2520 1530'%3E%3Cdefs%3E%3ClinearGradient id='a'%3E%3Cstop offset='0' stop-color='%23333'/%3E%3Cstop offset='.1' stop-color='%23aaa'/%3E%3Cstop offset='.8' stop-color='%23aaa'/%3E%3Cstop offset='1' stop-color='%23333'/%3E%3C/linearGradient%3E%3ClinearGradient id='d' x1='693' x2='693.6' y1='823.5' y2='917.5' gradientUnits='userSpaceOnUse' xlink:href='%23a'/%3E%3ClinearGradient id='c' x1='693' x2='693.6' y1='823.5' y2='917.5' gradientUnits='userSpaceOnUse' xlink:href='%23a'/%3E%3Cfilter id='b' width='1.3' height='2.8' x='-.1' y='-.9' color-interpolation-filters='sRGB'%3E%3CfeGaussianBlur stdDeviation='50.3'/%3E%3C/filter%3E%3Cfilter id='e' width='1.3' height='2.8' x='-.1' y='-.9' color-interpolation-filters='sRGB'%3E%3CfeGaussianBlur stdDeviation='50.3'/%3E%3C/filter%3E%3Cfilter id='f' width='1.3' height='2.8' x='-.1' y='-.9' color-interpolation-filters='sRGB'%3E%3CfeGaussianBlur stdDeviation='50.3'/%3E%3C/filter%3E%3C/defs%3E%3Cpath style='marker:none' fill='%231a1a1a' fill-opacity='.7' d='M1217 667c0 37-194 68-435 68-240 0-435-31-435-68s195-68 435-68c241 0 435 31 435 68z' color='%23000' enable-background='accumulate' filter='url(%23b)' overflow='visible' transform='matrix(2.02914 0 0 .71847 -50 822)'/%3E%3Cg transform='matrix(3.2252 0 0 3.2252 -554 -1612)'%3E%3Cpath style='marker:none' fill='url(%23d)' d='M641 823c-2 43-50 95-50 95h218s-49-52-50-95h-56z' color='%23000' enable-background='accumulate' overflow='visible' transform='translate(-51)'/%3E%3Crect style='marker:none' width='521.2' height='309.1' x='387.9' y='514' stroke='%23aaa' stroke-width='1.6' color='%23000' enable-background='accumulate' overflow='visible' rx='19' ry='18.3'/%3E%3Cpath style='marker:none' fill='%23fff' d='M409 534h480v270H409z' color='%23000' enable-background='accumulate' overflow='visible'/%3E%3Cpath style='marker:none' fill='%23aaa' d='M539 910h219v9H539z' color='%23000' enable-background='accumulate' overflow='visible'/%3E%3C/g%3E%3Cg color='%23000'%3E%3Cpath fill='%231a1a1a' fill-opacity='.7' d='M1217 667c0 37-194 68-435 68-240 0-435-31-435-68s195-68 435-68c241 0 435 31 435 68z' enable-background='accumulate' filter='url(%23e)' transform='matrix(.85876 0 0 .71847 92 907)' style='marker:none' overflow='visible'/%3E%3Cg transform='matrix(3.2252 0 0 3.2252 -368 -1606)'%3E%3Crect style='marker:none' width='200' height='266' x='247.1' y='671.4' stroke='%23aaa' stroke-width='.9' enable-background='accumulate' overflow='visible' rx='20' ry='20'/%3E%3Cpath style='marker:none' fill='%23fff' d='M264 687h167v222H264z' enable-background='accumulate' overflow='visible'/%3E%3C/g%3E%3C/g%3E%3Cg%3E%3Cpath fill='%231a1a1a' fill-opacity='.7' d='M1217 667c0 37-194 68-435 68-240 0-435-31-435-68s195-68 435-68c241 0 435 31 435 68z' color='%23000' enable-background='accumulate' transform='matrix(.34001 0 0 .71847 88 960)' style='marker:none' filter='url(%23f)' overflow='visible'/%3E%3Cg color='%23000' transform='translate(-768 -2231) scale(3.99062)'%3E%3Crect style='marker:none' width='70' height='130.3' x='247.1' y='797.1' stroke='%23aaa' stroke-width='.8' enable-background='accumulate' overflow='visible' rx='7' ry='9.8'/%3E%3Cpath style='marker:none' fill='%23fff' d='M252 805h60v106h-60z' enable-background='accumulate' overflow='visible'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E" alt="Placeholder image" width="600" height="365"/></figure></div>
                 <!-- /wp:image --></div></div>
                 <!-- /wp:group -->
 
-                <!-- wp:heading {"level":1} -->
-                <h1>[Name/Company Name]</h1>
+                <!-- wp:heading {"level":1,"className":"nf-placeholder nf-highlight"} -->
+                <h1 class="nf-placeholder nf-highlight">[Name/Company Name]</h1>
                 <!-- /wp:heading -->
 
                 <!-- wp:paragraph -->
-                <p>We’re dedicated to giving you the very best in [topic/product], [topic/product], and [topic/product]. Please reach out if you want to know more!</p>
+                <p>We’re dedicated to giving you the very best in <span class="nf-placeholder nf-highlight">[topic/product]</span>, <span class="nf-placeholder nf-highlight">[topic/product]</span>, and <span class="nf-placeholder nf-highlight">[topic/product]</span>. Please reach out if you want to know more!</p>
                 <!-- /wp:paragraph -->
 
                 <!-- wp:buttons -->
