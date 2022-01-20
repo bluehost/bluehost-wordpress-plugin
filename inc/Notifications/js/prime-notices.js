c7cbf8e16b648d898c00e248f855f77023a2dd6d
Dave Ryan <dave@bluehost.com>
2022-01-20 09:14:57 -0700

    semicolon for clarity

--- inc/Notifications/js/prime-notices.js
+++ inc/Notifications/js/prime-notices.js
@@ -4,7 +4,7 @@ const apiFetch = wp.apiFetch;
 const primeTransient = () => apiFetch({ path: '/bluehost/v1/notifications?context=wp-admin-prime'});
 
 if ('requestIdleCallback' in window) {
-    window.requestIdleCallback(primeTransient, { timeout: 2500 })
+    window.requestIdleCallback(primeTransient, { timeout: 2500 });
 } else {
     domReady(primeTransient);
 }
