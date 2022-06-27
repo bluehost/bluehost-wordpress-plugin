4caab5f8d003c59ed1f3177bc3a1f825412cdee7
aulisius <aulisius7@gmail.com>
2022-06-27 07:40:53 +0530

    Add actions that can be used by the module component

--- src/app/pages/ecommerce/index.js
+++ src/app/pages/ecommerce/index.js
@@ -1,5 +1,5 @@
 import { BWAAccountCard, BWAHelpCard } from "@app/components/molecules";
-import { useSelect } from "@wordpress/data";
+import { useDispatch, useSelect } from "@wordpress/data";
 import { withRouter } from "react-router-dom";
 import NewfoldECommerce from "../../../../vendor/newfold-labs/wp-module-ecommerce";
 
@@ -14,10 +14,14 @@ function EcommercePage() {
       isWooActive: wp.getSetting("isWooActive"),
     };
   });
-  const eCommerceState = { wp: brandPluginState }; 
+  const eCommerceState = { wp: brandPluginState };
+  const { toggleSetting } = useDispatch("bluehost/plugin");
+  const eCommerceActions = {
+    toggleComingSoon: async () => toggleSetting("comingSoon"),
+  };
   return (
     <div style={{ margin: "27px 42px 0 24px" }}>
-      <NewfoldECommerce state={eCommerceState} />
+      <NewfoldECommerce state={eCommerceState} actions={eCommerceActions} />
       <div className="grid-col-2">
         <BWAAccountCard />
         <BWAHelpCard />
