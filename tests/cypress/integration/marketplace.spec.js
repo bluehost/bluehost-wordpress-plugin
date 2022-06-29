f055b11f683745a06e6b152cc3d705ae0bfbf778
Evan Mullins <evanrm@gmail.com>
2022-06-29 14:35:26 -0400

    update marketplace e2e test

--- tests/cypress/integration/marketplace.spec.js
+++ tests/cypress/integration/marketplace.spec.js
@@ -1,5 +1,5 @@
 // <reference types="Cypress" />
-const marketplace = require('../fixtures/marketplace.json');
+const marketplace = require('../fixtures/products.json');
 
 describe('Marketplace Page', function () {
 
@@ -7,7 +7,6 @@ describe('Marketplace Page', function () {
 		cy.server();
 		cy.route('GET', '**/newfold-marketplace/v1/marketplace**', 'fx:marketplace').as('marketplace');
 		cy.visit('/wp-admin/admin.php?page=bluehost#/marketplace');
-		cy.wait('@marketplace');
 		cy.injectAxe();
 	});
 
@@ -20,66 +19,112 @@ describe('Marketplace Page', function () {
 		cy.checkA11y('.bwa-route-contents');
 	});
 
-	it('Product grid has 2 items', () => {
-		cy.get('.marketplace-item').should('have.length', 2);
+	it('Product grid has 12 items', () => {
+		cy.get('.marketplace-item').should('have.length', 12);
 	});
 
-	it('Product cards render correctly', () => {
-		cy.get('.marketplace-item-81288755-c7cf-4408-bc2f-8ac67020ba01').as('card');
+	it('First product card renders correctly', () => {
+		cy.get('#marketplace-item-0fd107dc-cfcc-4380-86ef-89a9ce01e443').as('card');
 
 		cy.get('@card')
-			.findByRole('link', {name: 'Add to my Account'})
+			.findByRole('link', {name: 'Learn More'})
 			.scrollIntoView()
 			.should('be.visible')
 			.should('have.attr', 'href')
-			.and('include', 'https://www.bluehost.com/blue-sky');
+			.and('include', 'https://www.bluehost.com/solutions/full-service');
 
 		cy.get('@card').first().within(() => {
-			cy.get('.components-card__header').should('be.visible');
+			cy.get('.components-card__header')
+				.contains('Web Design Services')
+				.should('be.visible');
 			cy.get('.components-card__media').should('be.visible');
+			cy.get('.components-card__header em.price').should('not.exist');
 		});
 	});
 
-	it('Product CTB cards render correctly', () => {
-		cy.get('.marketplace-item-ec14a614-8672-4094-8310-cb0b1eb0f176').as('card');
+	it('Second product card render correctly', () => {
+		cy.get('#marketplace-item-2a2e9bd4-3738-4f78-bed0-b67d2fc039f6').as('card');
 
 		cy.get('@card')
-			.findByRole('button', {name: 'Buy Now'})
+			.findByRole('link', {name: 'Buy Now'})
 			.scrollIntoView()
 			.should('be.visible')
-			.should('have.attr', 'data-action')
-			.and('include', 'load-nfd-ctb');
+			.should('have.attr', 'href')
+			.and('include', 'https://www.wpforms.com/');
 
 		cy.get('@card').first().within(() => {
-			cy.get('.components-card__header').should('be.visible');
+			cy.get('.components-card__header')
+				.contains('WPForms')
+				.should('be.visible');
 			cy.get('.components-card__media').should('be.visible');
+			cy.get('.components-card__header em.price')
+				.contains('$79.00')
+				.should('be.visible');
 		});
 	});
-
-	// it('Category Tab Filters properly', () => {
-	// 	cy.get('#tab-panel-0-Services').click();
-	// 	cy.wait(100);
-	// 	cy.get('.marketplace-item').should('have.length', 1);
-
-	// 	cy.get('.marketplace-item-81288755-c7cf-4408-bc2f-8ac67020ba01 h3')
-	// 		.scrollIntoView()
-	// 		.should('be.visible')
-	// 		.should('have.text', 'Bluesky');
-		
-	// 	cy.get('#tab-panel-0-Security').click();
-	// 		cy.wait(100);
-	// 		cy.get('.marketplace-item').should('have.length', 1);
 	
-	// 	cy.get('.marketplace-item-ec14a614-8672-4094-8310-cb0b1eb0f176 h3')
+	it('CTA links have target=_blank', () => {
+		cy.get('#marketplace-item-0fd107dc-cfcc-4380-86ef-89a9ce01e443').as('card');
+
+		cy.get('@card')
+			.findByRole('link', {name: 'Learn More'})
+			.scrollIntoView()
+			.should('have.attr', 'target')
+			.and('include', '_blank');
+	});
+
+	it('Load more button loads more products', () => {
+		cy.get('.marketplace-item').should('have.length', 12);
+
+		cy.contains('button', 'Load More');
+
+		cy.get('.marketplaceList button')
+			.scrollIntoView()
+			.click();
+
+		cy.wait(300);
+
+		cy.get('.marketplace-item').should('have.length', 18);
+	});
+
+	it('Category Tab Filters properly', () => {
+		cy.contains('button', 'Services');
+		cy.contains('button', 'SEO');
+
+		cy.get('#tab-panel-0-Services').click();
+		cy.wait(300);
+		cy.get('.marketplace-item').should('have.length', 6);
+
+		cy.get('#marketplace-item-0fd107dc-cfcc-4380-86ef-89a9ce01e443 h3')
+			.scrollIntoView()
+			.should('be.visible')
+			.should('have.text', 'Full Service');
+		
+		cy.get('#tab-panel-0-SEO').click();
+		cy.wait(300);
+		cy.get('.marketplace-item').should('have.length', 1);
+	
+		cy.get('#marketplace-item-7beee5ae-2e91-4282-9930-15ada43fc738 h3')
+			.scrollIntoView()
+			.should('be.visible')
+			.should('have.text', 'Yoast Premium');
+	});
+
+	// CTB Not included yet
+	// it('Product CTB cards render correctly', () => {
+	// 	cy.get('.marketplace-item-ec14a614-8672-4094-8310-cb0b1eb0f176').as('card');
+
+	// 	cy.get('@card')
+	// 		.findByRole('button', {name: 'Buy Now'})
 	// 		.scrollIntoView()
 	// 		.should('be.visible')
-	// 		.should('have.text', 'SSL');
-	// });
+	// 		.should('have.attr', 'data-action')
+	// 		.and('include', 'load-nfd-ctb');
 
-	// load more works
-	// secondary cta
-	// price
-	// categories properly set up into tabs
-	// cta target = _blank
+	// 	cy.get('@card').first().within(() => {
+	// 		cy.get('.components-card__header').should('be.visible');
+	// 		cy.get('.components-card__media').should('be.visible');
+	// 	});
+	// });
 
 });
