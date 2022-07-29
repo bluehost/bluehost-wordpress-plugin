75451ed30f68e83773ba7c19a32303febc919fb2
Micah Wood <micah@wpscholar.com>
2022-07-29 12:22:29 -0400

    Implement Replay.io

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const cypressReplay = require("@replayio/cypress");
+
 module.exports = (on, config) => {
+
+    cypressReplay.default(on, config);
 
     if (config.env && config.env.baseUrl) {
         config.baseUrl = config.env.baseUrl;
