afc8ca8a11bae77a5a044a417ca6d2eef8c895ba
A. Alani <abdulrahmanalani95@gmail.com>
2022-08-10 11:34:39 -0700

    Remove WP Admin Notices on Bluehost pages
    * Add remove_all_actions for all_admin_notices, admin_notices, and user_admin_notices with high specificity to override other calls.
    
    * Kept CSS script to count for notices that get injected after DOM load.

--- inc/admin-page-notifications-blocker.php
+++ inc/admin-page-notifications-blocker.php
@@ -18,7 +18,17 @@ class BH_Admin_Page_Notifications_Blocker {
 	 * BH_Admin_Page_Notifications_Blocker constructor.
 	 */
 	function __construct() {
+		add_action('admin_head', array( $this, 'remove_admin_notices_on_bluehost_pages' ), 99);
 		add_action( 'admin_print_styles', array( $this, 'remove_notifications_on_bluehost_pages' ) );
+	}
+
+	/**
+	 * Remove WP admin notices.
+	 */
+	function remove_admin_notices_on_bluehost_pages() {
+		remove_all_actions( 'all_admin_notices' );
+		remove_all_actions	( 'admin_notices' );
+		remove_all_actions( 'user_admin_notices' );
 	}
 
 	/**
