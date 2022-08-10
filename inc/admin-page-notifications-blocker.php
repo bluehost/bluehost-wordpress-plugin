b1ff81c223f063b354dbedc58664245c97c4516e
A. Alani <abdulrahmanalani95@gmail.com>
2022-08-10 12:47:38 -0700

    Fix linting issues

--- inc/admin-page-notifications-blocker.php
+++ inc/admin-page-notifications-blocker.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * Class containing functions for blocking WP admin notices and notifications on Bluehost pages.
+ *
+ * @package BluehostWordPressPlugin
+ */
 
 /**
- * Only run on bluehost pages
+ * Only run on Bluehost pages
  */
 if ( ! is_admin()
 	|| ! isset( $_GET['page'] )
@@ -17,24 +22,24 @@ class BH_Admin_Page_Notifications_Blocker {
 	/**
 	 * BH_Admin_Page_Notifications_Blocker constructor.
 	 */
-	function __construct() {
-		add_action('admin_head', array( $this, 'remove_admin_notices_on_bluehost_pages' ), 99);
+	public function __construct() {
+		add_action( 'admin_head', array( $this, 'remove_admin_notices_on_bluehost_pages' ), 99 );
 		add_action( 'admin_print_styles', array( $this, 'remove_notifications_on_bluehost_pages' ) );
 	}
 
 	/**
 	 * Remove WP admin notices.
 	 */
-	function remove_admin_notices_on_bluehost_pages() {
+	public function remove_admin_notices_on_bluehost_pages() {
 		remove_all_actions( 'all_admin_notices' );
-		remove_all_actions	( 'admin_notices' );
+		remove_all_actions( 'admin_notices' );
 		remove_all_actions( 'user_admin_notices' );
 	}
 
 	/**
 	 * Target notices using highly-specific CSS selectors to avoid collisions.
 	 */
-	function remove_notifications_on_bluehost_pages() {
+	public function remove_notifications_on_bluehost_pages() {
 		if ( ! isset( $_GET['page'] ) || false === stripos( filter_input( INPUT_GET, 'page' ), 'bluehost' ) ) {
 			return;
 		}
