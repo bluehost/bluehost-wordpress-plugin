21ce0af5e68814160c350158a33ea5aca43cada9
arunshenoy99 <devarunshenoy99@gmail.com>
2022-08-24 18:20:22 +0530

    PRESS2-165 test dymanic branding

---
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/branding.cy.js
@@ -0,0 +1,30 @@
+// <reference types="Cypress" />
+
+describe('Branding', function() {
+
+     before(() => {
+          cy.visit('wp-admin/index.php?page=nfd-onboarding&flow=ecommerce');
+	});
+
+     it('Has brand specific CSS', () => {
+          cy.exec('npx wp-env run cli wp option set mm_brand BlueHost');
+          cy.reload();
+          cy.get('body').should('have.class', 'nfd-brand-bluehost')
+     });
+
+     it('Has Newfold CSS when mm_brand has an empty value', () => {
+          cy.exec('npx wp-env run cli wp option set mm_brand ""');
+          cy.reload();
+          cy.get('body').should('have.class', 'nfd-brand-newfold')
+     });
+
+     it('Has Newfold CSS when mm_brand does not exist', () => {
+          cy.exec('npx wp-env run cli wp option delete mm_brand');
+          cy.reload();
+          cy.get('body').should('have.class', 'nfd-brand-newfold')
+     });
+
+     after(() => {
+          cy.exec('npx wp-env run cli wp option set mm_brand BlueHost');
+     })
+});
