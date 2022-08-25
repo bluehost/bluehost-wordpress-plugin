5155454dec7ebb2a5a363cafbdd347f273fd96ee
arunshenoy99 <devarunshenoy99@gmail.com>
2022-08-25 11:26:06 +0530

    PRESS2-165 spacing

--- tests/cypress/integration/newfold-labs/wp-module-onboarding/branding.cy.js
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/branding.cy.js
@@ -6,19 +6,19 @@ describe('Branding', function() {
           cy.visit('wp-admin/index.php?page=nfd-onboarding&flow=ecommerce');
 	});
 
-     it('Has brand specific CSS', () => {
+     it('Has brand specific CSS.', () => {
           cy.exec('npx wp-env run cli wp option set mm_brand BlueHost');
           cy.reload();
           cy.get('body').should('have.class', 'nfd-brand-bluehost')
      });
 
-     it('Has Newfold CSS when mm_brand has an empty value', () => {
+     it('Has Newfold CSS when mm_brand has an empty value.', () => {
           cy.exec('npx wp-env run cli wp option set mm_brand ""');
           cy.reload();
           cy.get('body').should('have.class', 'nfd-brand-newfold')
      });
 
-     it('Has Newfold CSS when mm_brand does not exist', () => {
+     it('Has Newfold CSS when mm_brand does not exist.', () => {
           cy.exec('npx wp-env run cli wp option delete mm_brand');
           cy.reload();
           cy.get('body').should('have.class', 'nfd-brand-newfold')
@@ -27,4 +27,5 @@ describe('Branding', function() {
      after(() => {
           cy.exec('npx wp-env run cli wp option set mm_brand BlueHost');
      })
+
 });
