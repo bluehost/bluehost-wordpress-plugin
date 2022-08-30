47cff5b0a0939f3e357b7a25b1df6a28908f389e
Micah Wood <micah@wpscholar.com>
2022-08-30 16:45:27 -0400

    Started cleaning up tests for welcome screen

--- tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/getstarted-welcome.cy.js
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/getstarted-welcome.cy.js
@@ -2,89 +2,89 @@
 
 describe('Get Started Welcome Page', function () {
 
-    before(() => {
-        cy.setCustomerData();
-        cy.visit('wp-admin/?page=nfd-onboarding&flow=ecommerce#/wp-setup/step/get-started/welcome');
-        // cy.injectAxe();
-        cy.wait(5000);
-    });
+	before(() => {
+		cy.setCustomerData()
 
-    // it('Is Accessible', () => {
-	// 	cy.wait(500);
-	// 	cy.checkA11y('.site-type-card');
-	// });
+		cy.visit('wp-admin/index.php?page=nfd-onboarding&flow=ecommerce')
+		cy.injectAxe()
+	})
 
-    it('Check Exit to Wordpress button is visible and clickable and continue flow', () => {
-        cy.get('.nfd-onboarding-etw__trigger')
-            .should('be.visible')
-            .click();
-        cy.get('.components-modal__screen-overlay').should('be.visible');
-        cy.get('.nfd-onboarding-etw__buttons > .is-secondary').click();
-        cy.get('.components-modal__screen-overlay').should('not.exist');
-    });
+	it('Starts at the welcome screen', () => {
+		cy.url({timeout: 20000}).should('include', '#/wp-setup/step/get-started/welcome')
+	})
 
-    it('Check Exit to Wordpress button is visible and clickable and cancel', () => {
-        cy.get('.nfd-onboarding-etw__trigger')
-            .should('be.visible')
-            .click();
-        cy.get('.components-modal__screen-overlay').should('be.visible');
-        cy.get('.components-modal__header > button').click();
-        cy.get('.components-modal__screen-overlay').should('not.exist');
-    });
+	it('Is Accessible', () => {
+		cy.checkA11y('#nfd-onboarding')
+	})
 
+	it('Drawer is closed and will not open', () => {
 
-    it('Check if Drawer is closed', () => {
-        cy.get('.nfd-onboarding-drawer__panel-inner').should('not.be.visible');
-    });
+		cy.get('.nfd-onboarding-drawer__panel-inner').as('drawer')
+		cy.get('[aria-label="Toggle Navigation"]').as('toggle')
 
-    it('Check to make sure drawer does not open', () => {
-        cy.get('.nfd-onboarding-drawer__toggle > .components-button').click();
-        cy.get('.nfd-onboarding-drawer__panel-inner')
-            .scrollIntoView()
-            .should('not.be.visible');
-    });
+		// Make sure drawer is closed
+		cy.get('@drawer').should('not.be.visible')
 
-    it('Check if welcome card is visible', () => {
-        cy.get('.welcome-card').should('be.visible');
-    });
+		// Click the toggle button
+		cy.get('@toggle').click()
 
-    it('Check if main heading and sub heading shows up', () => {
-        cy.get('.nfd-step-card-heading').should('be.visible');
-        cy.get('.nfd-step-card-subheading').should('be.visible');
-    });
+		// Make sure the drawer does not open
+		cy.get('@drawer').should('not.be.visible')
+	})
 
-    it('Check for brandname in sub heading', () => {
-        cy.exec('wp option set mm_brand BlueHost');
-        cy.reload();
-        cy.get('.nfd-step-card-subheading').should('contain', 'Bluehost');
-    });
+	it('Check if welcome card is visible', () => {
+		cy.get('.welcome-card').should('be.visible')
+	})
 
-    it('Change tab and check', () => {
-        cy.get('#tab-panel-0-tab2').trigger('mouseover').should('be.visible');
-        cy.get('#tab-panel-0-tab2-view').should('be.visible');
-    });
+	it('Check if main heading and sub heading shows up', () => {
+		cy.get('.nfd-step-card-heading').should('be.visible')
+		cy.get('.nfd-step-card-subheading').should('be.visible')
+	})
 
-    it('Check navigation back is not visible', () => {
-        cy.get('.navigation-buttons_back').should('not.exist');
-    });
+	it('Change tab and check', () => {
+		cy.get('#tab-panel-0-tab2').trigger('mouseover').should('be.visible')
+		cy.get('#tab-panel-0-tab2-view').should('be.visible')
+	})
 
-    it('Check if next step loads on clicking navigation next', () => {
-        cy.get('.navigation-buttons_next').click();
-        cy.url().should('not.include', '#/wp-setup/step/get-started/welcome');
-        cy.go('back');
-    });
+	it('"Back" button does not exist', () => {
+		cy.findByRole('button', {name: 'Back'}).should('not.exist')
+	})
 
-    it('Check Exit to Wordpress button is visible and clickable and exit flow', () => {
-        cy.get('.nfd-onboarding-etw__trigger')
-            .should('be.visible')
-            .click();
-        cy.get('.components-modal__screen-overlay').should('be.visible');
-        cy.get('.nfd-onboarding-etw__buttons > .is-primary').click();
-        cy.wait(3000);
-        cy.url().should('not.contain', '#/wp-setup/step/get-started/welcome');
-    });
+	it('"Next" button loads next screen on click', () => {
+		cy.findByRole('button', {name: 'Next'}).click()
+		cy.url().should('not.include', '#/wp-setup/step/get-started/welcome')
+		cy.go('back')
+	})
 
-    after(() => {
-        cy.clearCustomerData();
-    });
-});
+	it('"Exit to WordPress" button is visible and clickable and continue flow', () => {
+		cy.findByRole('button', {name: 'Exit to WordPress'})
+			.should('be.visible')
+			.click()
+		cy.get('.components-modal__screen-overlay').should('be.visible')
+		cy.get('.nfd-onboarding-etw__buttons > .is-secondary').click()
+		cy.get('.components-modal__screen-overlay').should('not.exist')
+	})
+
+	it('"Exit to WordPress" button is visible and clickable and cancel', () => {
+		cy.findByRole('button', {name: 'Exit to WordPress'})
+			.should('be.visible')
+			.click()
+		cy.get('.components-modal__screen-overlay').should('be.visible')
+		cy.get('.components-modal__header > button').click()
+		cy.get('.components-modal__screen-overlay').should('not.exist')
+	})
+
+	it('"Exit to Wordpress" button is visible and clickable and exit flow', () => {
+		cy.findByRole('button', {name: 'Exit to WordPress'})
+			.should('be.visible')
+			.click()
+		cy.get('.components-modal__screen-overlay').should('be.visible')
+		cy.get('.nfd-onboarding-etw__buttons > .is-primary').click()
+		cy.wait(3000)
+		cy.url().should('not.contain', '#/wp-setup/step/get-started/welcome')
+	})
+
+	after(() => {
+		cy.clearCustomerData()
+	})
+})
