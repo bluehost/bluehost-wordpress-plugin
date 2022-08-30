a1a6af91e652d8a09d8972c0e5b7d7fa36ce2f49
Micah Wood <micah@wpscholar.com>
2022-08-30 15:01:21 -0400

    Temporarily disable WP CLI commands since setting the values aren't working anyway

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -89,10 +89,12 @@ Cypress.Commands.add('deletePages', () => {
 });
 
 Cypress.Commands.add('setCustomerData', () => {
-     cy.exec(`npx wp-env run cli wp option update bh_cdata_guapi '${JSON.stringify(customerData)}'`);
+	// TODO: Replace this CLI command with direct manipulation of the Redux store.
+	//cy.exec(`npx wp-env run cli wp option update bh_cdata_guapi '${JSON.stringify(customerData)}'`);
 });
 
 Cypress.Commands.add('clearCustomerData', () => {
-     cy.exec('npx wp-env run cli wp option delete bh_cdata_guapi');
-     cy.exec('npx wp-env run cli wp transient delete bh_cdata');
+	// TODO: Replace this CLI command with direct manipulation of the Redux store.
+	//cy.exec('npx wp-env run cli wp option delete bh_cdata_guapi');
+	//cy.exec('npx wp-env run cli wp transient delete bh_cdata');
 });
