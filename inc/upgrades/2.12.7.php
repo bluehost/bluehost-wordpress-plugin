bdf41d39a64a6fc2a34751931f73fb4c1fb1e5c8
Evan Mullins <evanrm@gmail.com>
2022-09-22 15:00:10 -0400

    version bump

--- inc/upgrades/2.12.7.php
+++ inc/upgrades/2.12.7.php
@@ -5,7 +5,7 @@
  * @package Bluehost
  */
 
-// Clear bh_cdata throttle values with throttle fix in bluehost-wp-customer-data@1.5
+// Clear bh_cdata throttle values with throttle fix in bluehost-wp-customer-data@1.4.5
 if ( get_option( 'bh_cdata_retry_count' ) ) {
 	delete_option( 'bh_cdata_retry_count' );
 }
