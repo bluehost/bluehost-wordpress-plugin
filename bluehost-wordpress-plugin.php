96eab12ee579d861e3c2c2b15043555b35157edb
Evan Mullins <evanrm@gmail.com>
2023-01-17 11:57:43 -0500

    version bump

--- bluehost-wordpress-plugin.php
+++ bluehost-wordpress-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Bluehost
  * Description: This plugin integrates your WordPress site with the Bluehost control panel, including performance, security, and update features.
- * Version: 2.12.16
+ * Version: 2.12.17
  * Tested up to: 6.1.1
  * Requires at least: 5.9
  * Requires PHP: 7.0
@@ -33,7 +33,7 @@ if ( defined( 'BLUEHOST_PLUGIN_VERSION' ) ) {
 }
 
 // Define constants
-define( 'BLUEHOST_PLUGIN_VERSION', '2.12.16' );
+define( 'BLUEHOST_PLUGIN_VERSION', '2.12.17' );
 define( 'BLUEHOST_PLUGIN_FILE', __FILE__ );
 define( 'BLUEHOST_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BLUEHOST_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
