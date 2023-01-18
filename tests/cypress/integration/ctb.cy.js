ae9712d9a04b3ec7ac454d48c9d2e37fe6cab156
Evan Mullins <evanrm@gmail.com>
2023-01-18 12:02:10 -0500

    update rest route to non-permalink

--- tests/cypress/integration/ctb.cy.js
+++ tests/cypress/integration/ctb.cy.js
@@ -5,7 +5,7 @@
 describe('Click to buy', function () {
 
 	before(() => {
-		
+
 		cy.visit('/wp-admin/admin.php?page=bluehost#/marketplace/plugins/yoast-seo-premium');
 		// cy.injectAxe();
 
@@ -15,101 +15,101 @@ describe('Click to buy', function () {
 
 		cy.intercept({
 			method: 'GET',
-			url: '**?**bluehost/v1/ctb**'
-		}, {
+			url: '**?**bluehost/v1/ctb**',
+		},{
 			fixture: 'ctbGET.json'
 		}).as('ctbGET');
 
-        cy.intercept({
-            method: 'POST',
-            url: '**?**bluehost/v1/ctb**'
-        }, {
-            fixture: 'ctbPOST.json'
-        }).as('ctbPOST');
+		cy.intercept({
+			method: 'POST',
+			url: '**?**bluehost/v1/ctb**'
+		}, {
+			fixture: 'ctbPOST.json'
+		}).as('ctbPOST');
 
 		cy.visit('/wp-admin/admin.php?page=bluehost#/marketplace/plugins/yoast-seo-premium', {
-            // set nfdConnected to true, so the ctb click will pass to the endpoint and be intercepted
-            onBeforeLoad: (contentWindow) => {
-                contentWindow.nfdConnected = true
-            }
-        });
+			// set nfdConnected to true, so the ctb click will pass to the endpoint and be intercepted
+			onBeforeLoad: (contentWindow) => {
+				contentWindow.nfdConnected = true
+			}
+		});
 
 	});
 
 	it('Button has CTB Attributes', () => {
 		cy.get('[data-action="load-nfd-ctb"]')
-            .scrollIntoView()
-            .should('exist')
-            .should('be.visible')
-            .should('have.attr', 'data-ctb-id')
-            .and('equal', '57d6a568-783c-45e2-a388-847cff155897');
+			.scrollIntoView()
+			.should('exist')
+			.should('be.visible')
+			.should('have.attr', 'data-ctb-id')
+			.and('equal', '57d6a568-783c-45e2-a388-847cff155897');
 	});
 
 	it('CTB modal is functional', () => {
-        cy.get('body').should('not.have.class', 'noscroll');
+		cy.get('body').should('not.have.class', 'noscroll');
 
-        cy.get('[data-action="load-nfd-ctb"]')
-            .scrollIntoView()
-            .click();
-        // wait for intercept with data
-        cy.wait('@ctbGET');
+		cy.get('[data-action="load-nfd-ctb"]')
+			.scrollIntoView()
+			.click();
+		// wait for intercept with data
+		cy.wait('@ctbGET');
 
-        // check body for noscroll class
-        cy.get('body').should('have.class', 'noscroll');
+		// check body for noscroll class
+		cy.get('body').should('have.class', 'noscroll');
 
-        // check for modal should be.visible
-        cy.get('#nfd-ctb-container').should('exist');
-        cy.get('.ctb-modal-content')
-            .scrollIntoView()
-            .should('be.visible');
-        
-        // check for cancel button
-        cy.get('button[data-a11y-dialog-destroy]')    
-            .scrollIntoView()
-            .should('be.visible');
+		// check for modal should be.visible
+		cy.get('#nfd-ctb-container').should('exist');
+		cy.get('.ctb-modal-content')
+			.scrollIntoView()
+			.should('be.visible');
 
-        // check that cancel button closes modal
-        cy.get('button[data-a11y-dialog-destroy]').click();
-        cy.wait(200);
+		// check for cancel button
+		cy.get('button[data-a11y-dialog-destroy]')
+			.scrollIntoView()
+			.should('be.visible');
 
-        // confirm modal is closed
-        cy.get('body').should('not.have.class', 'noscroll');
-        cy.get('#nfd-ctb-container').should('have.attr', 'aria-hidden').and('equal', 'true');
-        cy.get('.ctb-modal-content').should('not.be.visible');
+		// check that cancel button closes modal
+		cy.get('button[data-a11y-dialog-destroy]').click();
+		cy.wait(200);
 
-        // reopen
-        cy.get('[data-action="load-nfd-ctb"]').click();
-        cy.wait(100);
-        cy.get('.ctb-modal-content')
-            .scrollIntoView()
-            .should('be.visible');
+		// confirm modal is closed
+		cy.get('body').should('not.have.class', 'noscroll');
+		cy.get('#nfd-ctb-container').should('have.attr', 'aria-hidden').and('equal', 'true');
+		cy.get('.ctb-modal-content').should('not.be.visible');
 
-        // confirm modal closes when overlay is clicked
-        cy.get('.ctb-modal-overlay').click();
-        cy.wait(100);
-        cy.get('.ctb-modal-content').should('not.be.visible');
+		// reopen
+		cy.get('[data-action="load-nfd-ctb"]').click();
+		cy.wait(100);
+		cy.get('.ctb-modal-content')
+			.scrollIntoView()
+			.should('be.visible');
+
+		// confirm modal closes when overlay is clicked
+		cy.get('.ctb-modal-overlay').click();
+		cy.wait(100);
+		cy.get('.ctb-modal-content').should('not.be.visible');
 	});
 
 	it('CTB purchase functions properly', () => {
-        // click CTB (again)
-        cy.get('[data-action="load-nfd-ctb"]').click();
+		// click CTB (again)
+		cy.get('[data-action="load-nfd-ctb"]').click();
 
-        // check for buy button
-        cy.get('[data-action="purchase-ctb"]')
-            .scrollIntoView()
-            .should('exist')
-            .should('be.visible')
-            .should('have.attr', 'data-ctb-id')
-            .and('equal', '57d6a568-783c-45e2-a388-847cff155897');
-        
-        // check that buy button hits POST endpoint - intercept
-        cy.get('[data-action="purchase-ctb"]').click();
-        cy.wait('@ctbPOST');
+		// check for buy button
+		cy.get('[data-action="purchase-ctb"]')
+			.scrollIntoView()
+			.should('exist')
+			.should('be.visible')
+			.should('have.attr', 'data-ctb-id')
+			.and('equal', '57d6a568-783c-45e2-a388-847cff155897');
 
-        // displays success
-        cy.get('.ctb-modal-content .nfd-ctb-modal-success')
-            .scrollIntoView()
-            .should('be.visible');
-    });
+		// check that buy button hits POST endpoint - intercept
+		cy.get('[data-action="purchase-ctb"]').click();
+		cy.wait('@ctbPOST');
+
+		// displays success
+		cy.get('.ctb-modal-content .nfd-ctb-modal-success')
+			.scrollIntoView()
+			.should('be.visible');
+	});
 
 });
