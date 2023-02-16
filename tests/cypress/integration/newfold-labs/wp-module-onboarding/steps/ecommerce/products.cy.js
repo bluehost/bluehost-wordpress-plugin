6ad859c2834845047fb73ce22985da4408fd3218
Allen Benny <allen.officially@gmail.com>
2023-02-16 12:48:03 +0530

    Update products.cy.js

--- tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/ecommerce/products.cy.js
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/ecommerce/products.cy.js
@@ -3,7 +3,7 @@
 describe('Step Ecommerce Products Info', function () {
     before(() => {
         // cy.setCustomerData();
-        cy.exec('npx wp-env run cli wp theme activate woocommerce');
+        cy.exec('npx wp-env run cli wp plugin activate woocommerce');
         cy.visit(
             'wp-admin/?page=nfd-onboarding&flow=ecommerce#/ecommerce/step/products'
         );
