16ef7a5511438ab0866dbc0ac477f585d12250fc
Evan Mullins <evanrm@gmail.com>
2023-03-28 17:54:36 -0400

    skip new tests again (for now)

--- cypress.config.js
+++ cypress.config.js
@@ -53,6 +53,7 @@ module.exports = defineConfig({
 		supportFile: 'tests/cypress/support/index.js',
 		testIsolation: false,
 		excludeSpecPattern: [
+			"tests/cypress/integration/newfold-labs/wp-module-onboarding/",
 			"tests/cypress/integration/newfold-labs/wp-module-onboarding/wp-module-support/"
 		]
 	},
