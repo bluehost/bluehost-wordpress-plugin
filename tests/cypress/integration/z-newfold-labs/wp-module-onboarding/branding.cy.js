a452c2e8ae93d8e5082662576a0eb80a9d109fa1
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2023-04-20 17:32:39 +0530

    fixing test when mm_brand does not exist in DB

--- tests/cypress/integration/z-newfold-labs/wp-module-onboarding/branding.cy.js
+++ tests/cypress/integration/z-newfold-labs/wp-module-onboarding/branding.cy.js
@@ -5,14 +5,16 @@ describe( 'Branding', function () {
 		cy.visit( 'wp-admin/index.php?page=nfd-onboarding&flow=ecommerce' );
 	} );
 
-	it( 'Has Newfold CSS when mm_brand does not exist.', () => {
+	// since we are setting brand from plugin container, it will not be set to "newfold"
+	// by default even if mm_brand option is deleted from the database
+	it( 'Has Bluehost CSS when mm_brand does not exist.', () => {
 		cy.exec( 'npx wp-env run cli wp option delete mm_brand' );
 		cy.reload();
-		cy.get( 'body' ).should( 'have.class', 'nfd-brand-newfold' );
+		cy.get( 'body' ).should( 'have.class', 'nfd-brand-bluehost' );
 		cy.get( '.is-bg-primary' )
-			.should( 'have.css', 'background-color', 'rgb(221, 82, 40)' )
-			.should( 'have.css', 'color', 'rgb(242, 242, 242)' );
-		cy.get( '.nfd-step-card-subheading' ).should( 'contain', 'undefined' );
+			.should( 'have.css', 'background-color', 'rgb(53, 117, 211)' )
+			.should( 'have.css', 'color', 'rgb(255, 255, 255)' );
+		cy.get( '.nfd-step-card-subheading' ).should( 'contain', 'Bluehost' );
 	} );
 
 	it( 'Has Newfold CSS when mm_brand has an empty value', () => {
