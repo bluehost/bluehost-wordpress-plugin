310f9814e10de2df9afb863c86a7d6f41a8735c4
aulisius <aulisius7@gmail.com>
2023-05-23 21:47:11 +0530

    Read from window as context isn't ready

--- src/app/components/site-info/index.js
+++ src/app/components/site-info/index.js
@@ -1,12 +1,10 @@
 import { LockClosedIcon, LockOpenIcon } from "@heroicons/react/24/outline";
-import { useContext } from "@wordpress/element";
 import { Button } from "@yoast/ui-library";
-import AppStore from "../../data/store";
 import { BluehostIcon, WordPressIcon } from "../icons";
 
 export const SiteInfoBar = () => {
-    const { store } = useContext( AppStore );
-    const parsedUrl = new URL(store.siteurl);
+    const { siteurl, sitetitle } = window.WPPB;
+    const parsedUrl = new URL(siteurl);
     const siteDomain = parsedUrl.hostname;
     const hasSSL = parsedUrl.protocol.includes("https");
 
@@ -23,7 +21,7 @@ export const SiteInfoBar = () => {
             <div className="yst-flex yst-items-center yst-gap-4">
 
                 <div className="yst-grow yst-flex yst-flex-col yst-gap-1.5">
-                    <h3 className="yst-text-white yst-text-2xl yst-font-semibold">{store.sitetitle}</h3>
+                    <h3 className="yst-text-white yst-text-2xl yst-font-semibold">{sitetitle}</h3>
                     <div className="yst-flex yst-items-center yst-gap-3 yst-font-medium">
                         <div className="yst-flex yst-items-center yst-gap-1">
                             {renderPadLock()}
@@ -37,7 +35,7 @@ export const SiteInfoBar = () => {
                         <BluehostIcon />
                         Bluehost Account
                     </Button>
-                    <Button as="a" href={store.siteurl} target="_blank" variant="primary" className="yst-bg-white yst-text-[#212936] yst-text-tiny">
+                    <Button as="a" href={siteurl} target="_blank" variant="primary" className="yst-bg-white yst-text-[#212936] yst-text-tiny">
                         <WordPressIcon />
                         View Site
                     </Button>
