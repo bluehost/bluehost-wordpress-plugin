f4aa847d16e8c6a5a21fe12681d875711ca3de38
aulisius <aulisius7@gmail.com>
2023-05-24 20:56:12 +0530

    Use heroicons

--- src/app/components/icons/index.js
+++ src/app/components/icons/index.js
@@ -1,8 +1,2 @@
 export { default as BluehostIcon } from './BluehostIcon';
 export { default as WordPressIcon } from './WordPressIcon';
-export { ReactComponent as BillingIcon } from './svg/billing.svg';
-export { ReactComponent as MailIcon } from './svg/mail.svg';
-export { ReactComponent as ProductsIcon } from './svg/products.svg';
-export { ReactComponent as SecurityIcon } from './svg/security.svg';
-export { ReactComponent as UserIcon } from './svg/user.svg';
-export { ReactComponent as ValidationIcon } from './svg/validation.svg';
--- src/app/pages/home/accountCard.js
+++ src/app/pages/home/accountCard.js
@@ -1,54 +1,55 @@
-import { Card, Title } from "@yoast/ui-library";
 import {
-  BillingIcon,
-  MailIcon,
-  ProductsIcon,
-  SecurityIcon,
-  UserIcon,
-  ValidationIcon,
-} from "../../components/icons";
+  CpuChipIcon,
+  CreditCardIcon,
+  EnvelopeIcon,
+  GiftIcon,
+  IdentificationIcon,
+  ShieldCheckIcon,
+} from "@heroicons/react/24/outline";
+import { Card, Title } from "@yoast/ui-library";
 import { getPlatformBaseUrl, getPlatformPathUrl } from "../../util/helpers";
+import classNames from "classnames";
 
 const isJarvis = window.WPPB.env.isJarvis;
 
 const base = [
   {
-    icon: UserIcon,
+    icon: CpuChipIcon,
     id: "account_link",
     href: getPlatformPathUrl("home", "app"),
     label: __("Control Panel", "bluehost-wordpress-plugin"),
     color: "yst-fill-gray",
   },
   {
-    icon: BillingIcon,
-    id: "billing_link",
-    href: getPlatformPathUrl("renewal-center", "account_center#billing"),
-    label: __("Billing", "bluehost-wordpress-plugin"),
-    color: "yst-fill-primary",
-  },
-  {
-    icon: ProductsIcon,
+    icon: GiftIcon,
     id: "products_link",
     href: getPlatformPathUrl("market-place", "account_center#products"),
     label: __("Products", "bluehost-wordpress-plugin"),
     color: "yst-fill-primary-dark",
   },
   {
-    icon: MailIcon,
+    icon: CreditCardIcon,
+    id: "billing_link",
+    href: getPlatformPathUrl("renewal-center", "account_center#billing"),
+    label: __("Billing", "bluehost-wordpress-plugin"),
+    color: "yst-fill-primary",
+  },
+  {
+    icon: EnvelopeIcon,
     id: "mail_link",
     href: getPlatformPathUrl("home", "app#/email-office"),
     label: __("Mail & Office", "bluehost-wordpress-plugin"),
     color: "yst-fill-[#5b5b5b]",
   },
   {
-    icon: SecurityIcon,
+    icon: ShieldCheckIcon,
     id: "security_link",
     href: getPlatformPathUrl("security", "account_center#security"),
     label: __("Security", "bluehost-wordpress-plugin"),
     color: "yst-fill-[#17b212]",
   },
   {
-    icon: ValidationIcon,
+    icon: IdentificationIcon,
     id: "validation_token_link",
     href: isJarvis
       ? getPlatformPathUrl("account-center")
@@ -63,7 +64,7 @@ export function AccountCard(props) {
     <Card>
       <Card.Content>
         <Title size={2}>Bluehost Account</Title>
-        <ul className="yst-grid yst-grid-cols-3 yst-h-full yst-mt-3">
+        <ul className="yst-grid yst-grid-cols-3 yst-h-full">
           {base.map((link) => (
             <li
               key={link.id}
@@ -71,11 +72,13 @@ export function AccountCard(props) {
             >
               <a
                 href={link.href}
-                className={
-                  "yst-flex yst-flex-col yst-items-center yst-text-center yst-text-primary"
-                }
+                className={classNames(
+                  "yst-flex yst-flex-col yst-gap-3",
+                  "yst-items-center yst-text-center",
+                  "yst-text-[#404040] hover:yst-text-primary"
+                )}
               >
-                <link.icon className={"yst-w-12 " + link.color} />
+                <link.icon className={"yst-w-12"} />
                 {link.label}
               </a>
             </li>
