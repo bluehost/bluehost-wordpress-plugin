2f1c8113ebf9fad07690e9f4340bbae12bc74410
Evan Mullins <evanrm@gmail.com>
2023-08-01 11:25:26 -0400

    use createRoot in place of render - react 18 update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './webpack-public-path';
 
 import App from './app';
 import domReady from '@wordpress/dom-ready';
-import { render } from '@wordpress/element';
+import { createRoot } from '@wordpress/element';
 
 const WP_ADM_PAGE_ROOT_ELEMENT = 'wppbh-app';
 const W_ASCI = `
@@ -30,7 +30,8 @@ console.log( W_ASCI );
 const WPPBHRender = () => {
 	const DOM_ELEMENT = document.getElementById( WP_ADM_PAGE_ROOT_ELEMENT );
 	if ( null !== DOM_ELEMENT && 'undefined' !== typeof render ) {
-		render( <App />, DOM_ELEMENT );
+		const root = createRoot( DOM_ELEMENT );
+		root.render( <App /> );
 	}
 };
 
