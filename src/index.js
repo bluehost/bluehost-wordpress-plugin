51b08b7c985a1bd3658e8303549caa9cdf07e57c
Micah Wood <micah@wpscholar.com>
2023-08-03 09:21:04 -0400

    Clean up conditional logic for `createRoot` vs `render`

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './webpack-public-path';
 import App from './app';
 import domReady from '@wordpress/dom-ready';
 import {createRoot, render} from '@wordpress/element';
-import {NewfoldRuntime} from "@newfold-labs/wp-module-runtime";
-import semver from 'semver';
 
 const WP_ADM_PAGE_ROOT_ELEMENT = 'wppbh-app';
 const W_ASCI = `
@@ -31,19 +29,12 @@ console.log(W_ASCI);
 
 const WPPBHRender = () => {
     const DOM_ELEMENT = document.getElementById(WP_ADM_PAGE_ROOT_ELEMENT);
-    const wpVersion = NewfoldRuntime?.sdk?.wpversion;
-    const WP_VERSION = wpVersion.split('.').length !== 3 ? `${wpVersion}.0` : wpVersion;
     if (null !== DOM_ELEMENT) {
-        if ('undefined' !== typeof render && semver.lt(WP_VERSION, '6.2.0')) {
-            console.log('render');
-            render(
-                <App/>,
-                DOM_ELEMENT
-            );
-        }
-        if ('undefined' !== typeof createRoot && semver.gte(WP_VERSION, '6.2.0')) {
-            console.log('createRoot');
+        if ('undefined' !== typeof createRoot) {
+            // WP 6.2+ only
             createRoot(DOM_ELEMENT).render(<App/>);
+        } else if ('undefined' !== typeof render) {
+            render(<App/>, DOM_ELEMENT);
         }
     }
 };
