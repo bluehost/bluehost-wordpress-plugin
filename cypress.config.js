45d6b7cfef4a0a7f94a988c3328ed16f56632cef
Avneet Raj <razavneet@gmail.com>
2023-08-04 15:53:44 +0530

    MS Teams integration with Cypress Tests workflow

--- cypress.config.js
+++ cypress.config.js
@@ -25,6 +25,15 @@ module.exports = defineConfig({
 		'*jnn-pa.googleapis.com',
 		'*youtube.com',
 	],
+	reporter: 'mochawesome',
+	reporterOptions: {
+		reportDir: "tests/cypress/report/mochawesome",
+		overwrite: true,
+		charts: true,
+		html: false,
+		json: true,
+		timestamp: "mmddyyyy_HHMMss"
+		},
   e2e: {
 		setupNodeEvents(on, config) {
 			const semver = require('semver');
