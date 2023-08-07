f1a32e5ae63b79d7e12ee07d52ccbf1144d60a2b
sumathi3399 <sumathivarma3399@gmail.com>
2023-08-07 13:20:23 +0530

    PRESS4-303 | Added payments tab in store

--- src/app/data/routes.js
+++ src/app/data/routes.js
@@ -89,6 +89,12 @@ export const routes = [
 				title: __("Sales & Discounts", "wp-plugin-bluehost"),
 			}
 			: null,
+			NewfoldRuntime.hasCapability( 'isEcommerce' )
+			? {
+				name: '/store/payments',
+				title: __( 'Payments', 'wp-plugin-bluehost' ),
+			}
+			: null,
 			{
 				name: '/store/details',
 				title: __( 'Store Details', 'wp-plugin-bluehost' ),
