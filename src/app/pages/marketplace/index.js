097701c30d517e42d6e361f1974a001aa8846344
Evan Mullins <evanrm@gmail.com>
2023-08-23 19:04:22 -0400

    update to marketplace module

--- src/app/pages/marketplace/index.js
+++ src/app/pages/marketplace/index.js
@@ -1,11 +1,10 @@
 import apiFetch from '@wordpress/api-fetch';
-import classnames from 'classnames';
 import { useState, useEffect } from '@wordpress/element';
 import { useLocation } from 'react-router-dom';
-import { SectionContainer, SectionHeader, SectionContent } from "../../components/section";
+import classnames from 'classnames';
 import { Page } from "../../components/page";
+import { SectionContainer, SectionHeader, SectionContent } from "../../components/section";
 import { NewfoldRuntime } from "@newfold-labs/wp-module-runtime";
-
 // component sourced from marketplace module
 import { default as NewfoldMarketplace } from '../../../../vendor/newfold-labs/wp-module-marketplace/components/marketplace/';
 
@@ -13,10 +12,15 @@ const MarketplacePage = () => {
 
     // constants to pass to module
     const moduleConstants = {
-        'eventendpoint': '/newfold-data/v1/events/',
-        'perPage': 12,
-        'supportsCTB': true,
-    };
+		'supportsCTB': true,
+		'text': {
+			'title': __('Marketplace', 'bluehost-wordpress-plugin'),
+			'subTitle': __('Explore our featured collection of tools and services.', 'bluehost-wordpress-plugin'),
+			'error': __('Oops, there was an error loading the marketplace, please try again later.', 'bluehost-wordpress-plugin'),
+			'noProducts': __('Sorry, no marketplace items. Please, try again later.', 'bluehost-wordpress-plugin'),
+			'loadMore': __('Load More', 'bluehost-wordpress-plugin'),
+		}
+	};
 
     // methods to pass to module
     const moduleMethods = {
@@ -28,22 +32,21 @@ const MarketplacePage = () => {
         NewfoldRuntime,
     };
 
+	const moduleComponents = {
+		SectionHeader,
+		SectionContent,
+	}
+
     return (
         <Page className={"wppbh-app-marketplace-page"}>
             <SectionContainer className={'wppbh-app-marketplace-container'}>
-                <SectionHeader
-                    title={__('Marketplace', 'wp-plugin-bluehost')}
-                    subTitle={__('Explore our featured collection of tools and services.', 'wp-plugin-bluehost')}
-                    className={'wppbh-app-marketplace-header'}
+
+                <NewfoldMarketplace 
+                    methods={moduleMethods}
+                    constants={moduleConstants}
+                    Components={moduleComponents}
                 />
-                <SectionContent className={'wppbh-app-marketplace-content'}>
 
-                    <NewfoldMarketplace 
-                        methods={moduleMethods}
-                        constants={moduleConstants}
-                    />
-
-                </SectionContent>
             </SectionContainer>
         </Page>
     );
