58d39b8970275c7668d14e11c0f54b21b374a5be
wpalani <abdulrahmanalani95@gmail.com>
2023-10-03 21:37:32 -0700

    Restore cypress configs
    Accidentally pushed my local's cypress configs

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,8 @@ const wpVersion = /[^/]*$/.exec(core)[0]
 module.exports = defineConfig({
   projectId: "h78f39",
   env: {
-    wpUsername: 'Alani',
-    wpPassword: 'Admin@123',
+    wpUsername: 'admin',
+    wpPassword: 'password',
     wpVersion,
     phpVersion,
 	pluginId: 'bluehost',
@@ -76,7 +76,7 @@ module.exports = defineConfig({
 
 			return config;
 		},
-    baseUrl: 'http://localhost:10041',
+    baseUrl: 'http://localhost:8882',
 		specPattern: [
 			"tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
 			"vendor/newfold-labs/**/tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
