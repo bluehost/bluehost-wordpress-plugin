43e0c48e82beb284464f1e95c58caa00765809e0
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-10-13 11:43:55 +0530

    to show sales & discounts based on ``canAccessGlobalCTB``

--- src/app/data/routes.js
+++ src/app/data/routes.js
@@ -84,7 +84,7 @@ export const routes = [
 				name: '/store/products',
 				title: __( 'Products & Services', 'wp-plugin-bluehost' ),
 			},
-			NewfoldRuntime.hasCapability( 'hasYithExtended' )
+			NewfoldRuntime.hasCapability( 'hasYithExtended' ) || (NewfoldRuntime.hasCapability('canAccessGlobalCTB') && (NewfoldRuntime.sdk.ecommerce.brand_settings.brand === "bluehost"))
 			? {
 				name: "/store/sales_discounts",
 				title: __("Sales & Discounts", "wp-plugin-bluehost"),
