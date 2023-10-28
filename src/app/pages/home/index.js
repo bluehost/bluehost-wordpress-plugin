0455430b23b31546a48e20927eafccd6d7afa671
wpalani <abdulrahmanalani95@gmail.com>
2023-10-28 14:47:59 -0700

    Bluehost webinars ad banner

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -1,5 +1,6 @@
 import { Page } from '../../components/page';
 import { SectionContainer, SectionContent } from '../../components/section';
+import WebinarsBanner from 'App/components/webinars-banner';
 import { AccountCard } from './accountCard';
 import { HelpCard } from './helpCard';
 import './stylesheet.scss';
@@ -9,6 +10,7 @@ const Home = () => {
 	return (
 		<Page className="wppbh-home">
 			<WelcomeSection />
+			<WebinarsBanner />
 			<SectionContainer className="wppbh-account-help-section">
 				<SectionContent >
 					<div className="nfd-grid nfd-grid-cols-2 nfd-gap-6">
