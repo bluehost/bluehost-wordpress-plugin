f1ae376db85be44fb428cb9df766cd3deaae210b
Evan Mullins <evanrm@gmail.com>
2023-12-06 12:18:47 -0500

    update WP version to 6.4.2

--- bluehost-wordpress-plugin.php
+++ bluehost-wordpress-plugin.php
@@ -15,7 +15,7 @@
  * Version:           3.7.0
  * Requires at least: 6.0
  * Requires PHP:      7.1
- * Tested up to:      6.4.0
+ * Tested up to:      6.4.2
  * Author:            Bluehost
  * Author URI:        https://bluehost.com
  * Text Domain:       wp-plugin-bluehost
