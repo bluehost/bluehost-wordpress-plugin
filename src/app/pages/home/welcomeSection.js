227a098da230817b87892dd579a3d236aa553ef0
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-06-24 12:12:38 +0530

    calling comingSoonAdminbarToggle on update of comingsoon

--- src/app/pages/home/welcomeSection.js
+++ src/app/pages/home/welcomeSection.js
@@ -1,8 +1,10 @@
 import { useContext } from '@wordpress/element';
+import { useUpdateEffect } from 'react-use';
 import { OnboardingScreen } from '@newfold-labs/wp-module-ecommerce';
 import AppStore from 'App/data/store';
 import { bluehostSettingsApiFetch } from 'App/util/helpers';
 import { useNotification } from 'App/components/notifications';
+import { comingSoonAdminbarToggle } from '../../util/helpers';
 
 const WelcomeSection = ( {} ) => {
 	const { store, setStore } = useContext( AppStore );
@@ -15,6 +17,10 @@ const WelcomeSection = ( {} ) => {
 			console.error,
 			() => setStore( { ...store, comingSoon: ! store.comingSoon } )
 		);
+
+		useUpdateEffect(() => {
+			comingSoonAdminbarToggle(store.comingSoon)
+		}, [store.comingSoon])
 
 	return (
 		<OnboardingScreen
