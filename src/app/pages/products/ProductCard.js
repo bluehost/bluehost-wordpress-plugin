819640ac846ccb22210de10ec39d83060283f2f6
lokapure.girish <lokapure.girish@gmail.com>
2024-06-28 14:30:56 +0530

    added check for jarvis site

--- src/app/pages/products/ProductCard.js
+++ src/app/pages/products/ProductCard.js
@@ -1,5 +1,5 @@
 import { Card, Table } from '@newfold/ui-component-library';
-import { productApiFetch } from '../../util/helpers';
+import { productApiFetch, isJarvis } from '../../util/helpers';
 
 const formatDate = ( dateString ) => {
 	const date = new Date( dateString );
@@ -20,47 +20,48 @@ const determineMessage = ( autoRenewFlag, expirationDate ) => {
 const ProductCard = ( { props } ) => {
 	const [ productData, setProductData ] = useState( '' );
 	useEffect( () => {
-		productApiFetch( [], console.error, ( response ) => {
-			setProductData( response );
-		} );
+		if ( isJarvis() ) {
+			productApiFetch( [], console.error, ( response ) => {
+				setProductData( response );
+			} );
+		}
 	}, [] );
 	return (
 		<Card { ...props }>
 			<Card.Content>
 				{ productData && (
 					<Table>
-						<React.Fragment key=".0">
-							<Table.Head>
-								<Table.Row>
-									<Table.Header>
-										Products & Services
-									</Table.Header>
-									<Table.Header>Renewal Date</Table.Header>
-									<Table.Header>Renewal Setting</Table.Header>
+						<Table.Head>
+							<Table.Row>
+								<Table.Header>Products & Services</Table.Header>
+								<Table.Header>Renewal Date</Table.Header>
+								<Table.Header>Renewal Setting</Table.Header>
+							</Table.Row>
+						</Table.Head>
+						<Table.Body>
+							{ productData.map( ( product ) => (
+								<Table.Row key={ product.prodId }>
+									<Table.Cell>
+										{ product.prodName }
+									</Table.Cell>
+									<Table.Cell>
+										{ determineMessage(
+											product.autoRenewFlag,
+											product.expirationDate
+										) }
+									</Table.Cell>
+									<Table.Cell>
+										<a href="https://www.bluehost.com/my-account/renewal-center">
+											Manage Renewal
+										</a>
+									</Table.Cell>
 								</Table.Row>
-							</Table.Head>
-							<Table.Body>
-								{ productData.map( ( product ) => (
-									<Table.Row key={ product.prodId }>
-										<Table.Cell>
-											{ product.prodName }
-										</Table.Cell>
-										<Table.Cell>
-											{ determineMessage(
-												product.autoRenewFlag,
-												product.expirationDate
-											) }
-										</Table.Cell>
-										<Table.Cell>
-											<a href="https://www.bluehost.com/my-account/renewal-center">
-												Manage Renewal
-											</a>
-										</Table.Cell>
-									</Table.Row>
-								) ) }
-							</Table.Body>
-						</React.Fragment>
+							) ) }
+						</Table.Body>
 					</Table>
+				) }
+				{ ! isJarvis() && (
+					<p>Please login to your account manager to see products</p>
 				) }
 			</Card.Content>
 		</Card>
