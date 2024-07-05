942640edc063291427287fec7a5cb4244dcf706c
lokapure.girish <lokapure.girish@gmail.com>
2024-07-05 11:18:36 +0530

    updated module import and small improments

--- src/app/pages/products/index.js
+++ src/app/pages/products/index.js
@@ -1,20 +1,13 @@
 import { Container, Page } from '@newfold/ui-component-library';
 import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect } from '@wordpress/element';
-import { useLocation, useMatch, useNavigate } from 'react-router-dom';
-import classnames from 'classnames';
 import { NewfoldRuntime } from '@newfold-labs/wp-module-runtime';
-import { default as NewfoldProducts } from '@newfold-labs/wp-module-products/components/product';
+import { default as NewfoldProducts } from '@newfold-labs/wp-module-my-products/components/product';
 import { isJarvis } from '../../util/helpers';
 
 // constants to pass to module
 const moduleConstants = {
 	text: {
-		title: __( 'My Products', 'bluehost-wordpress-plugin' ),
-		subTitle: __(
-			'Explore our featured collection of tools and services.',
-			'bluehost-wordpress-plugin'
-		),
 		error: __(
 			'Oops, there was an error loading products, please try again later.',
 			'bluehost-wordpress-plugin'
@@ -23,18 +16,18 @@ const moduleConstants = {
 			'Sorry, no products. Please, try again later.',
 			'bluehost-wordpress-plugin'
 		),
+		jarvisText: __(
+			'Please login to your account manager to see products.',
+			'bluehost-wordpress-plugin'
+		),
 	},
 };
 
 // methods to pass to module
 const moduleMethods = {
 	apiFetch,
-	classnames,
 	useState,
 	useEffect,
-	useLocation,
-	useMatch,
-	useNavigate,
 	NewfoldRuntime,
 	isJarvis,
 };
@@ -43,6 +36,23 @@ const Products = () => {
 	return (
 		<Page className="wppbh-products" title="Active Products">
 			<Container className="wppbh-products-section">
+				<Container.Header
+					title={ __( 'My Products', 'bluehost-wordpress-plugin' ) }
+				>
+					<p>
+						{ __(
+							'A list of products associated with your Bluehost hosting package. For more information, check out the ',
+							'bluehost-wordpress-plugin'
+						) }
+
+						<a href="https://www.bluehost.com/my-account/renewal-center">
+							{ __(
+								'Bluehost Account Manager',
+								'bluehost-wordpress-plugin'
+							) }
+						</a>
+					</p>
+				</Container.Header>
 				<NewfoldProducts
 					methods={ moduleMethods }
 					constants={ moduleConstants }
