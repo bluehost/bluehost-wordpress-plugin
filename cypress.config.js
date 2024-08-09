4d3af38684faebc45f87c0033fc1032e7d7f7f6e
arunshenoy99 <devarunshenoy99@gmail.com>
2024-08-09 17:38:12 +0530

    Skip Next Step Novice User tests for WP < 6.5

--- cypress.config.js
+++ cypress.config.js
@@ -58,6 +58,7 @@ module.exports = defineConfig( {
 				config.excludeSpecPattern = config.excludeSpecPattern.concat( [
 					'vendor/newfold-labs/wp-module-ecommerce/tests/cypress/integration/Site-Capabilities/**',
 					'vendor/newfold-labs/wp-module-ecommerce/tests/cypress/integration/Home/homePageWithWoo.cy.js',
+					'vendor/newfold-labs/wp-module-ecommerce/tests/cypress/integration/Home/ecommerce-next-steps.cy.js', // Skip this since Onboarding does not support this version
 					'vendor/newfold-labs/wp-module-onboarding/tests/cypress/integration/**' // Onboarding requires WP 6.5 or greater, as it uses the Wonder Theme which has the same requirement
 				] );
 			}
