ea788aedf46b9a53fb1e2585cdc2e94d88fff46c
wpalani <abdulrahmanalani95@gmail.com>
2024-09-03 18:45:38 -0700

    Fix oatterns module path

--- cypress.config.js
+++ cypress.config.js
@@ -66,7 +66,7 @@ module.exports = defineConfig( {
 			// Exclude WonderBlocks tests for WordPress lower than 6.6 (6.5 or 6.4)
 			if ( semver.satisfies( config.env.wpSemverVersion, '<6.6.0' ) ) {
 				config.excludeSpecPattern = config.excludeSpecPattern.concat( [
-					'vendor/newfold-labs/wp-module-wonderblocks/tests/cypress/integration/**',
+					'vendor/newfold-labs/wp-module-patterns/tests/cypress/integration/**',
 				] );
 			}
 
