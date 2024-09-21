9ff4c0b0f3971e9947ffcdef645910b0fa5e9492
Dave Ryan <dave@bluehost.com>
2024-09-21 12:07:14 -0700

    Temporarily remove revision settings UI and filter

--- inc/settings.php
+++ inc/settings.php
@@ -18,7 +18,7 @@ namespace Bluehost;
 function nfd_settings_revisions( $num, $post ) {
 	return get_option( 'nfd_wp_post_revisions', 5 );
 }
-add_filter( 'wp_revisions_to_keep', __NAMESPACE__ . '\\nfd_settings_revisions', 10, 2 );
+// add_filter( 'wp_revisions_to_keep', __NAMESPACE__ . '\\nfd_settings_revisions', 10, 2 );
 
 if ( ! defined( 'EMPTY_TRASH_DAYS' ) ) {
 	$nfd_empty_trash_days = get_option( 'nfd_empty_trash_days', 30 );
