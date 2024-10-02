01c36fe25f7e9526513bfce24dfc7a0c828f739a
Evan Mullins <evanrm@gmail.com>
2024-10-02 14:21:11 -0400

    update to use buildless component

--- src/app/data/routes.js
+++ src/app/data/routes.js
@@ -16,7 +16,7 @@ import Home from '../pages/home';
 import PagesAndPosts from '../pages/pages-and-posts';
 import Store from '../pages/ecommerce/page';
 import Marketplace from '../pages/marketplace';
-import WPSolution from '../pages/wpsolutions';
+import Solutions from '../pages/solutions';
 import Performance from '../pages/performance';
 import Settings from '../pages/settings';
 import Staging from '../pages/staging';
