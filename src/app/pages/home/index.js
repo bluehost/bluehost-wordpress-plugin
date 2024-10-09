ed407fed778ae1d4bb36525fc19622cb6c1a0c4c
Evan Mullins <evanrm@gmail.com>
2024-10-09 10:49:07 -0400

    update ecommerce module to solutions release 1.4.0-beta
    remove old component import

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -3,7 +3,6 @@ import WebinarsBanner from 'App/components/webinars-banner';
 import AccountCard from './accountCard';
 import HelpCard from './helpCard';
 import WelcomeSection from './welcomeSection';
-import MyProductSection from './myProductsSection';
 import { WPSolutionsBanner } from '@newfold-labs/wp-module-ecommerce';
 
 const Home = () => {
