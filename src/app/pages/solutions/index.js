514ca94a23d80f66d9ffba0f92b27f46c6ea838c
aratidgr8 <aratidgr8@gmail.com>
2024-10-10 17:59:40 +0530

    Eslint fix src/app/pages/solutions/index.js

--- src/app/pages/solutions/index.js
+++ src/app/pages/solutions/index.js
@@ -2,7 +2,7 @@ import './style.scss';
 import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect } from '@wordpress/element';
 import { useLocation, useMatch, useNavigate } from 'react-router-dom';
-import { Container, Page, Title } from '@newfold/ui-component-library';
+import { Container, Page } from '@newfold/ui-component-library';
 import { NewfoldRuntime } from '@newfold-labs/wp-module-runtime';
 import { default as NewfoldEntitlements } from '@modules/wp-module-solutions/components/entitlements';
 
